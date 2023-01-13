47c041284bfd88fc3360050fcfa1bd14bd6e6c7e
thisisnew <thisisnew@naver.com>
2023-01-13 17:53:53 +0900

    프로그래머스 개인정보 수집 유효기간 레벨1 자바로 풀어보기

---
+++ src/programmers/개인정보수집유효기간.java
@@ -0,0 +1,38 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 개인정보수집유효기간 {
+    public static void main(String[] args) {
+        Privacy privacy = new Privacy();
+        System.out.println(privacy.solution("",new String[]{}, new String[]{}));
+    }
+
+
+}
+
+class Privacy {
+    int[] solution(String today, String[] terms, String[] privacies) {
+
+        Map<String,Integer> termsMap = getValidityMap(terms);
+
+
+
+        int[] answer = {};
+        return answer;
+    }
+
+    public Map<String, Integer> getValidityMap(String[] terms) {
+
+        Map<String, Integer> result = new HashMap<>();
+
+        for(String term : terms) {
+            String[] t = term.split(" ");
+            result.put(t[0],Integer.parseInt(t[1]));
+        }
+
+        return result;
+    }
+
+}
