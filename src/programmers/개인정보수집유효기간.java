c288eb803f0f836e8fbee5c25e95c16a92dc476f
thisisnew <thisisnew@naver.com>
2023-01-14 23:10:21 +0900

    프로그래머스 개인정보 수집 유효기간 레벨1 자바로 풀어보기

--- src/programmers/개인정보수집유효기간.java
+++ src/programmers/개인정보수집유효기간.java
@@ -10,8 +10,6 @@ public class 개인정보수집유효기간 {
         Privacy privacy = new Privacy();
         System.out.println(privacy.solution("", new String[]{}, new String[]{}));
     }
-
-
 }
 
 class Privacy {
@@ -25,16 +23,12 @@ class Privacy {
             String collectionDate = entities[0];
             String termsType = entities[1];
 
-            result.add(i + 1);
+            if (isAbrogateDate(today, "")) {
+                result.add(i + 1);
+            }
         }
 
-        int[] answer = new int[result.size()];
-
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
-
-        return answer;
+        return result.stream().mapToInt(i -> i).toArray();
     }
 
     private Map<String, Integer> getValidityMap(String[] terms) {
@@ -49,8 +43,51 @@ class Privacy {
         return result;
     }
 
+//    private String getExpirationDate() {
+//
+//    }
+
     private String[] getPrivacyEntities(String privacy) {
         return privacy.split(" ");
+    }
+
+    private int[] getDayEntities(String day) {
+        String[] entities = day.split("[.]");
+
+        int[] result = new int[entities.length];
+
+        for (int i = 0; i < entities.length; i++) {
+            result[i] = Integer.parseInt(entities[i]);
+        }
+
+        return result;
+    }
+
+    private boolean isAbrogateDate(String today, String expireDate) {
+
+        int[] todayEntities = getDayEntities(expireDate);
+        int thisYear = todayEntities[0];
+        int thisMonth = todayEntities[1];
+        int thisDay = todayEntities[2];
+
+        int[] expireDateEntities = getDayEntities(expireDate);
+        int expireYear = expireDateEntities[0];
+        int expireMonth = expireDateEntities[1];
+        int expireDay = expireDateEntities[2];
+
+        if (thisYear < expireYear) {
+            return false;
+        }
+
+        if (thisYear == expireYear && thisMonth < expireMonth) {
+            return false;
+        }
+
+        if (thisYear == expireYear && thisMonth == expireMonth && thisDay < expireDay) {
+            return false;
+        }
+
+        return true;
     }
 
 }
