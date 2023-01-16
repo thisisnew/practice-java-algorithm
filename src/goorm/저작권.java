2411ea06c77d82fd3df4dd0db8437a2c30397726
thisisnew <thisisnew@naver.com>
2023-01-17 00:31:55 +0900

    저작권

---
+++ src/goorm/저작권.java
@@ -0,0 +1,18 @@
+package goorm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 저작권 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+
+        System.out.println(a * (l - 1) + 1);
+    }
+}
