64beb789ec9bd5c196c599920b9eef3074e47c7e
thisisnew <thisisnew@naver.com>
2023-01-19 10:23:49 +0900

    두부 자르기

---
+++ src/goorm/두부자르기.java
@@ -0,0 +1,20 @@
+package goorm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 두부자르기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<Integer, Integer> slice = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+
+        }
+    }
+}
