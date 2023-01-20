a017ace13e3c7ba1175fa4a3dd843aec255e7265
thisisnew <thisisnew@naver.com>
2023-01-20 20:25:42 +0900

    같은 숫자는 싫어

---
+++ src/programmers/같은숫자는싫어.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 같은숫자는싫어 {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+
+        같은숫자는싫어solution(arr);
+    }
+
+    private static int[] 같은숫자는싫어solution(int[] arr) {
+        List<Integer> list = Arrays.asList(arr[0]);
+
+        int std = list.get(0);
+        for (int i = 1; i < arr.length; i++) {
+`           if(arr[i] != std ){
+                list.add(arr[i]);
+            }
+        }
+
+        int[] result = new int[list.size()];
+
+        for(int i=0; i<list.size(); i++){
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+}
