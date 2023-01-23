43ddb1ea5b365c3799dce1bbab9040fe5846fcee
thisisnew <thisisnew@naver.com>
2023-01-23 15:08:41 +0900

    비밀지도

--- src/programmers/비밀지도.java
+++ src/programmers/비밀지도.java
@@ -5,54 +5,33 @@ public class 비밀지도 {
         int n = 5;
         int[] arr1 = {9, 20, 28, 18, 11};
         int[] arr2 = {30, 1, 21, 17, 28};
-
-        System.out.println(solution(n, arr1, arr2));
+        
+        SecretMap scMap = new SecretMap();
+        System.out.println(scMap.solution(n, arr1, arr2));
     }
-
-    private static String[] solution(int n, int[] arr1, int[] arr2) {
-
-        String[][] secretMap = new String[n][n];
-
-        int idx = 0;
-        for (int i = 0; i < arr1.length; i++) {
-            String bin = Integer.toBinaryString(arr1[i]);
-
-            for (int j = 0; j < bin.length(); j++) {
-                secretMap[idx][i] = String.valueOf(bin.charAt(j));
-            }
-
-            idx++;
-        }
-
-
-        String[] answer = {};
-        return answer;
-    }
-
-    private static
 }
 
 class SecretMap {
     public String[] solution(int n, int[] arr1, int[] arr2) {
 
-        String[][] secretMap = mark(unionBetweenTwoArrays(arr1,arr2),n);
+        String[][] secretMap = mark(unionBetweenTwoArrays(arr1, arr2), n);
 
         String[] answer = {};
         return answer;
     }
 
-    private int[] unionBetweenTwoArrays(int[] arr1, int[] arr2){
+    private int[] unionBetweenTwoArrays(int[] arr1, int[] arr2) {
 
         int len = Math.max(arr1.length, arr2.length);
 
         int[] result = new int[len];
 
-        for(int i=0; i<arr1.length; i++){
-            result[i] = result[i]==1 ? result[i] : arr1[i];
+        for (int i = 0; i < arr1.length; i++) {
+            result[i] = result[i] == 1 ? result[i] : arr1[i];
         }
 
-        for(int i=0; i<arr2.length; i++){
-            result[i] = result[i]==1 ? result[i] : arr1[i];
+        for (int i = 0; i < arr2.length; i++) {
+            result[i] = result[i] == 1 ? result[i] : arr1[i];
         }
 
         return result;
