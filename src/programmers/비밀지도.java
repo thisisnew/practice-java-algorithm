3cff1002a79bac1e8ca1c48c58602b5f8b40757b
thisisnew <thisisnew@naver.com>
2023-01-23 10:48:39 +0900

    비밀지도

--- src/programmers/비밀지도.java
+++ src/programmers/비밀지도.java
@@ -11,20 +11,64 @@ public class 비밀지도 {
 
     private static String[] solution(int n, int[] arr1, int[] arr2) {
 
-        String[][] map = new String[n][n];
+        String[][] secretMap = new String[n][n];
 
         int idx = 0;
         for (int i = 0; i < arr1.length; i++) {
             String bin = Integer.toBinaryString(arr1[i]);
 
             for (int j = 0; j < bin.length(); j++) {
-                //map[idx][j] = bin.charAt(j) - 'A';
+                secretMap[idx][i] = String.valueOf(bin.charAt(j));
             }
+
             idx++;
         }
 
 
         String[] answer = {};
         return answer;
+    }
+
+    private static
+}
+
+class SecretMap {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+
+        String[][] secretMap = new String[n][n];
+
+        String[] answer = {};
+        return answer;
+    }
+
+    private int[] unionBetweenTwoArrays(int[] arr1, int[] arr2){
+
+        int len = Math.max(arr1.length, arr2.length);
+
+        int[] result = new int[len];
+
+        for(int i=0; i<arr1.length; i++){
+            result[i] = arr1[i];
+        }
+
+        for(int i=0; i<arr2.length; i++){
+
+        }
+
+        return result;
+    }
+
+    private void mark(String[][] secretMap, int[] arr) {
+        int idx = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            String bin = Integer.toBinaryString(arr[i]);
+
+            for (int j = 0; j < bin.length(); j++) {
+                secretMap[idx][i] = String.valueOf(bin.charAt(j));
+            }
+
+            idx++;
+        }
     }
 }
