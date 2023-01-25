9a96a19357e16bda2cf6018570a92ca014a932a7
thisisnew <thisisnew@naver.com>
2023-01-26 00:00:32 +0900

    단어장 만들기

---
+++ src/goorm/단어장만들기.java
@@ -0,0 +1,37 @@
+package goorm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class 단어장만들기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+        }
+
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+
+                return o1.length() - o2.length();
+            }
+        });
+
+        System.out.println(words[k - 1]);
+    }
+}
