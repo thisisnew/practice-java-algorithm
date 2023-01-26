47c48487dd699588e253da6308960648f42964e2
thisisnew <thisisnew@naver.com>
2023-01-27 00:35:26 +0900

    다트 게임

---
+++ src/programmers/다트게임.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class 다트게임 {
+    public static void main(String[] args) {
+        DartGame dart = new DartGame();
+        System.out.println(dart.solution(""));
+    }
+}
+
+class DartGame {
+    public int solution(String dartResult) {
+
+        String first = "";
+        String second = "";
+        for (int i = 0; i < dartResult.length(); i++) {
+
+        }
+
+        int answer = 0;
+        return answer;
+    }
+}
+
