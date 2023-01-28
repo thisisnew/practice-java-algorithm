2875173714b873656e981c7a30203531f06b574a
thisisnew <thisisnew@naver.com>
2023-01-29 00:42:31 +0900

    다트 게임

--- src/programmers/다트게임.java
+++ src/programmers/다트게임.java
@@ -10,15 +10,36 @@ public class 다트게임 {
 class DartGame {
     public int solution(String dartResult) {
 
-        String first = "";
-        String second = "";
-        
+        int[] points = new int[3];
+        int idx = 0;
+
         for (int i = 0; i < dartResult.length(); i++) {
+
+            String s = Character.toString(dartResult.charAt(i));
+
+            if(isNumeric(s)) {
+               points[idx] = Integer.parseInt(s);
+               continue;
+            }
+
+
 
         }
 
         int answer = 0;
         return answer;
     }
+
+    private boolean isNumeric(String s) {
+
+        try {
+            Integer.parseInt(s);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true
+    }
+
 }
 
