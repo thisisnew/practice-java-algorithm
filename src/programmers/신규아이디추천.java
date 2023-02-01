d2844d1ead0003fde5dabce25b8631e6708b923c
thisisnew <thisisnew@naver.com>
2023-02-02 00:39:36 +0900

    신규 아이디 추천

---
+++ src/programmers/신규아이디추천.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class 신규아이디추천 {
+
+    public static void main(String[] args) {
+        NewId newId = new NewId();
+        System.out.println(newId.solution("...!@BaT#*..y.abcdefghijklm"));
+    }
+
+}
+
+class NewId {
+    public String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+}
