9e2d49e924205061697167a89288496351a698e1
thisisnew <thisisnew@naver.com>
2023-02-03 00:27:22 +0900

    둘만의 암호

---
+++ src/programmers/둘만의암호.java
@@ -0,0 +1,31 @@
+package programmers;
+
+public class 둘만의암호 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class PassWord {
+    public String solution(String s, String skip, int index) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            sb.append("");
+        }
+
+        return sb.toString();
+    }
+
+    private String getCharacterAfterIndex(int index) {
+        String result = "";
+        int cnt = 0;
+
+        while (cnt < index) {
+
+        }
+
+        return result;
+    }
+}
