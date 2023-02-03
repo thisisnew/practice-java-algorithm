b29a8552d84f5ce53313f028c02dad090a8abba3
thisisnew <thisisnew@naver.com>
2023-02-04 00:06:40 +0900

    키패드 누르기

---
+++ src/programmers/키패드누르기.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class 키패드누르기 {
+    public static void main(String[] args) {
+        KeyPad keyPad = new KeyPad();
+        int[] numbers = new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        String hand = "right";
+        System.out.println(keyPad.solution(numbers, hand));
+    }
+}
+
+class KeyPad {
+    public String solution(int[] numbers, String hand) {
+        String answer = "";
+        return answer;
+    }
+}
