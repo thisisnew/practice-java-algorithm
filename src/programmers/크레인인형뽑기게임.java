cfa6268dfefb88adddcb7f52d7656e7f2ce85c26
thisisnew <thisisnew@naver.com>
2023-02-15 00:00:58 +0900

    크레인 인형 뽑기 게임

--- src/programmers/크레인인형뽑기게임.java
+++ src/programmers/크레인인형뽑기게임.java
@@ -1,5 +1,8 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 크레인인형뽑기게임 {
     public static void main(String[] args) {
         int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
@@ -13,6 +16,29 @@ public class 크레인인형뽑기게임 {
 class ToyCraneGame {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
+        List<Integer> basket = new ArrayList<>();
+
+        for (int move : moves) {
+            int doll = getDoll(board, move);
+        }
+
         return answer;
+    }
+
+    private int getDoll(int[][] board, int index) {
+        int result = 0;
+        int[] dolls = board[index];
+
+        for (int i = 0; i < dolls.length; i++) {
+            if (dolls[i] == 0) {
+                continue;
+            }
+
+            result = dolls[i];
+            dolls[i] = 0;
+            break;
+        }
+
+        return result;
     }
 }
