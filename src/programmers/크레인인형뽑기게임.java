4ebfaeba0b09738741c01df7e9f96170d2bbbada
thisisnew <thisisnew@naver.com>
2023-02-18 00:08:46 +0900

    크레인 인형 뽑기 게임

--- src/programmers/크레인인형뽑기게임.java
+++ src/programmers/크레인인형뽑기게임.java
@@ -19,9 +19,10 @@ class ToyCraneGame {
 
         for (int move : moves) {
             int doll = getDoll(board, move);
-
-            if (!basket.isEmpty() && basket.get(basket.size() - 1) == doll) {
-                basket.remove(basket.size() - 1);
+            
+            int top = basket.size() - 1;
+            if (!basket.isEmpty() && basket.get(top) == doll) {
+                basket.remove(top);
                 continue;
             }
 
