e8487359258e2d1bf596fa06cf5152c3469412dd
thisisnew <thisisnew@naver.com>
2023-02-21 01:12:27 +0900

    카드 뭉치

--- src/programmers/카드뭉치.java
+++ src/programmers/카드뭉치.java
@@ -19,9 +19,42 @@ class Deck {
 
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = YES;
-        
+
+        for (String g : goal) {
+            int idx = findIndexIfHasSameProperty(cards1, g);
+
+            if (idx >= 0) {
+                cards1 = removePropInArray(cards1, idx);
+                continue;
+            }
+
+
+            idx = findIndexIfHasSameProperty(cards2, g);
+
+            if (idx >= 0) {
+                cards2 = removePropInArray(cards2, idx);
+                continue;
+            }
+
+            answer = NO;
+            break;
+        }
 
         return answer;
+    }
+
+    private int findIndexIfHasSameProperty(String[] cards, String g) {
+
+        int result = -1;
+
+        for (int i = 0; i < cards.length; i++) {
+            if (cards[i] == g) {
+                result = i;
+                break;
+            }
+        }
+
+        return result;
     }
 
     private String[] removePropInArray(String[] cards, int idx) {
