41959ce438aaaa329d9ae551d53e4af5a1396ee3
thisisnew <thisisnew@naver.com>
2023-02-21 01:05:56 +0900

    카드 뭉치

--- src/programmers/카드뭉치.java
+++ src/programmers/카드뭉치.java
@@ -7,14 +7,37 @@ public class 카드뭉치 {
         String[] cards1 = {"i", "drink", "water"};
         String[] cards2 = {"want", "to"};
         String[] goal = {"i", "want", "to", "drink", "water"};
-        
+
         System.out.println(deck.solution(cards1, cards2, goal));
     }
 }
 
 class Deck {
+
+    private final static String YES = "Yes";
+    private final static String NO = "No";
+
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        String answer = "";
+        String answer = YES;
+        
+
         return answer;
+    }
+
+    private String[] removePropInArray(String[] cards, int idx) {
+
+        String[] result = new String[cards.length - 1];
+        int index = 0;
+
+        for (int i = 0; i < cards.length; i++) {
+
+            if (i == idx) {
+                continue;
+            }
+
+            result[index] = cards[idx];
+        }
+
+        return result;
     }
 }
