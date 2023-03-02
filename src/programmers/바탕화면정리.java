8e4cc24481b81ba6b3daf95168720a3376bee4de
thisisnew <thisisnew@naver.com>
2023-03-03 00:01:04 +0900

    바탕화면 정리

---
+++ src/programmers/바탕화면정리.java
@@ -0,0 +1,35 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 바탕화면정리 {
+    public static void main(String[] args) {
+        WallPaper wallPaper = new WallPaper();
+        String[] wallpaper = {".#...", "..#..", "...#."};
+        System.out.println(Arrays.toString(wallPaper.solution(wallpaper)));
+    }
+}
+
+class WallPaper {
+    public int[] solution(String[] wallpaper) {
+
+        int minIndex = 0;
+
+        for (String w : wallpaper) {
+            for (int i = 0; i < w.length(); i++) {
+                if (w.charAt(i) != '#') {
+                    continue;
+                }
+
+                if (i <= minIndex) {
+                    minIndex = i;
+                    break;
+                }
+            }
+        }
+
+
+        int[] answer = {};
+        return answer;
+    }
+}
