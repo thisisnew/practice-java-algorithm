1ddd70489995c953d3881c6ff6668e88f75362cb
thisisnew <thisisnew@naver.com>
2023-04-08 00:36:43 +0900

    달리기경주

--- src/programmers/달리기경주.java
+++ src/programmers/달리기경주.java
@@ -1,6 +1,8 @@
 package programmers;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class 달리기경주 {
     public static void main(String[] args) {
@@ -14,7 +16,24 @@ public class 달리기경주 {
 
 class RunningRace {
     public String[] solution(String[] players, String[] callings) {
+        Map<String, Integer> playersRankMap = getPlayersRankMap(players);
+
+        for (String calling : callings) {
+
+        }
+
         String[] answer = {};
         return answer;
+    }
+
+    private Map<String, Integer> getPlayersRankMap(String[] players) {
+
+        Map<String, Integer> playersRank = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            playersRank.put(players[i], i);
+        }
+
+        return playersRank;
     }
 }
