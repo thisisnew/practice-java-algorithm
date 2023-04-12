dc792e7af3ad1f9ead641410f7c8dfe69b6e8516
thisisnew <thisisnew@naver.com>
2023-04-12 22:53:56 +0900

    공원 산책

---
+++ src/programmers/공원산책.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class 공원산책 {
+    public static void main(String[] args) {
+        WalkingPark walkingPark = new WalkingPark();
+        String[] park = {"SOO", "OOO", "OOO"};
+        String[] routes = {"E 2", "S 2", "W 1"};
+        System.out.println(walkingPark.solution(park, routes));
+    }
+}
+
+class WalkingPark {
+    public int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
+        return answer;
+    }
+    
+}
