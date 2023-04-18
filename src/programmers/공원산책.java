a54ea5e31851f8235efd2c00e834cfaec43c4311
thisisnew <thisisnew@naver.com>
2023-04-18 21:10:42 +0900

    공원 산책

--- src/programmers/공원산책.java
+++ src/programmers/공원산책.java
@@ -16,10 +16,30 @@ class WalkingPark {
     public int[] solution(String[] park, String[] routes) {
 
         for (String route : routes) {
+            goStep(park, route);
         }
 
         int[] answer = {};
         return answer;
+    }
+
+    private boolean[][] getTwoDimensionsPark(String[] park) {
+
+        int width = park[0].length();
+        int height = park.length;
+
+        boolean[][] result = new boolean[width][height];
+
+        for (int i = 0; i < park.length; i++) {
+            for (int j = 0; j < park[i].length(); j++) {
+
+                if (park[i].charAt(j) == 'S') {
+
+                }
+
+            }
+        }
+
     }
 
     private void goStep(String[] park, String route) {
