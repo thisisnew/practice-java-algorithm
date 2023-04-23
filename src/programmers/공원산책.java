b8094d3fa8e676139b98bb9aaa2bd0f6f2570924
thisisnew <thisisnew@naver.com>
2023-04-23 15:58:01 +0900

    공원 산책

--- src/programmers/공원산책.java
+++ src/programmers/공원산책.java
@@ -1,13 +1,14 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class 공원산책 {
     public static void main(String[] args) {
         WalkingPark walkingPark = new WalkingPark();
-        String[] park = {"SOO", "OOO", "OOO"};
+        String[] park = {"SOO", "OXX", "OOO"};
         String[] routes = {"E 2", "S 2", "W 1"};
-        System.out.println(walkingPark.solution(park, routes));
+        System.out.println(Arrays.toString(walkingPark.solution(park, routes)));
     }
 }
 
@@ -15,23 +16,25 @@ class WalkingPark {
 
     int wStart = 0;
     int hStart = 0;
+    int width = 0;
+    int height = 0;
+
+    boolean[][] twoDimensionsPark = null;
 
     public int[] solution(String[] park, String[] routes) {
-
-        boolean[][] twoDimensionsPark = getTwoDimensionsPark(park);
+        twoDimensionsPark = getTwoDimensionsPark(park);
 
         for (String route : routes) {
-            goStep(park, route);
+            goStep(route);
         }
 
-        int[] answer = {};
-        return answer;
+        return new int[]{wStart, hStart};
     }
 
     private boolean[][] getTwoDimensionsPark(String[] park) {
 
-        int width = park[0].length();
-        int height = park.length;
+        width = park[0].length();
+        height = park.length;
 
         boolean[][] result = new boolean[width][height];
 
@@ -48,20 +51,45 @@ class WalkingPark {
         return result;
     }
 
-    private void goStep(String[] park, String route) {
+    private void goStep(String route) {
         StringTokenizer st = new StringTokenizer(route);
         String direction = st.nextToken();
-        String steps = st.nextToken();
+        int steps = Integer.parseInt(st.nextToken());
 
         switch (direction) {
             case "E":
+                if (wStart + steps <= width - 1) {
+                    wStart += steps;
+                }
                 break;
             case "W":
+                if (wStart - steps >= 0) {
+                    wStart -= steps;
+                }
                 break;
             case "S":
+                if (hStart + steps >= height - 1) {
+                    hStart += steps;
+                }
                 break;
             case "N":
+                if (hStart - steps >= 0) {
+                    hStart -= steps;
+                }
                 break;
         }
+    }
+
+    private boolean hasObstacle(int start, int steps, String operator) {
+
+        if (operator == "+") {
+
+
+        } else {
+
+        }
+
+
+        return false;
     }
 }
