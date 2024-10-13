59c251c177bf5c34d73037300a6b791f38e95ccc
thisisnew <thisisnew@naver.com>
2024-10-13 11:57:26 +0900

    1120번 문자열

--- src/baekjoon/string/No1120.java
+++ src/baekjoon/string/No1120.java
@@ -3,13 +3,18 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class No1120 {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));\
-        String a = br.readLine();
-        String b = br.readLine();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(input);
+
+        String a = tokenizer.nextToken();
+        String b = tokenizer.nextToken();
+
         int result = Integer.MAX_VALUE;
 
         int aLen = a.length();
