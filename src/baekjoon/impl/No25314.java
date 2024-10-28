6b5fcf321cd241282db29c03963a6eabe951abc5
thisisnew <thisisnew@naver.com>
2024-10-28 23:56:08 +0900

    25314번 코딩은 체육과목 입니다

--- src/baekjoon/impl/No25314.java
+++ src/baekjoon/impl/No25314.java
@@ -1,7 +1,15 @@
 package baekjoon.impl;
 
-public class No25314 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class No25314 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+
+        
     }
 }
