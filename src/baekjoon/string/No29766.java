bd114784b8264aef91e18c99c863bf435bd9405d
thisisnew <thisisnew@naver.com>
2025-03-25 20:27:14 +0900

    29766번 DKSH 찾기 개선

--- src/baekjoon/string/No29766.java
+++ src/baekjoon/string/No29766.java
@@ -6,21 +6,26 @@ import java.io.InputStreamReader;
 
 public class No29766 {
 
-    private static final String INITIAL = "DKSH";
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
+        char[] inputs = br.readLine().toCharArray();
 
         int count = 0;
 
-        for (int i = 0; i < input.length() - 3; i++) {
-            if (INITIAL.equals(input.substring(i, i + 4))) {
+        for (int i = 0; i < inputs.length - 3; i++) {
+            if (matches(inputs, i)) {
                 count++;
             }
         }
 
         System.out.println(count);
+    }
+
+    private static boolean matches(char[] inputs, int i) {
+        return inputs[i] == 'D' &&
+                inputs[i + 1] == 'K' &&
+                inputs[i + 2] == 'S' &&
+                inputs[i + 3] == 'H';
     }
 }
