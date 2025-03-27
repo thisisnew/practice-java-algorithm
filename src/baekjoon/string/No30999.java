5a42108b3ee9cd14a9f6e6a1aae08653f92059e6
thisisnew <thisisnew@naver.com>
2025-03-27 18:45:54 +0900

    30999번 민주주의

---
+++ src/baekjoon/string/No30999.java
@@ -0,0 +1,25 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No30999 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+        System.out.println(count);
+    }
+}
