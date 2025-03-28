dc1e0e694b7906ad1a5c0c137192d4a49dc873b5
thisisnew <thisisnew@naver.com>
2025-03-29 08:18:58 +0900

    11575번 Affine Cipher

--- src/baekjoon/string/No11575.java
+++ src/baekjoon/string/No11575.java
@@ -9,29 +9,29 @@ public class No11575 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());   // 문제 수
-        int m = Integer.parseInt(st.nextToken());   // 출제위원
-        int half = (m + 1) / 2;
-        int result = 0;
+        int t = Integer.parseInt(br.readLine());   // 테스트케이스 수
 
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
-            int count = 0;
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            for(char c : s.toCharArray()) {
-                if(c == 'O') {
-                    count++;
+            String plainText = br.readLine();
+            System.out.println(encrypt(a, b, plainText));
+        }
+    }
 
-                    if (count == half) {
-                        result++;
-                        break;
-                    }
-                }
-            }
+    private static String encrypt(int a, int b, String plainText) {
+
+        StringBuilder result = new StringBuilder();
+
+        for(char c : plainText.toCharArray()) {
+            int x = c - 'A';
+            int encrypted = (a * x + b) % 26;
+            result.append((char) ('A' + encrypted));;
         }
 
-        System.out.println(result);
+        return result.toString();
     }
 }
