50972e4299121b26a9e653e084d676fd3ac4bcd1
thisisnew <thisisnew@naver.com>
2025-03-30 19:53:12 +0900

    30402번 감마선을 맞은 컴퓨터

--- src/baekjoon/string/No30402.java
+++ src/baekjoon/string/No30402.java
@@ -3,42 +3,29 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class No30402 {
 
+    private static final Map<String, String> NAME_MAP = Map.of(
+            "w", "chunbae",
+            "b", "nabi",
+            "g", "yeongcheol"
+    );
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        boolean find = false;
 
-        for(int i = 0; i<15; i++) {
+        for (int i = 0; i < 15; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            for (int j = 0; j < st.countTokens(); j++) {
-                String s = st.nextToken();
+            while (st.hasMoreTokens()) {
+                String token = st.nextToken();
 
-                switch (s) {
-                    case "w" -> {
-                        System.out.println("chunbae");
-                        find = true;
-                    }
-                    case "b" -> {
-                        System.out.println("nabi");
-                        find = true;
-                    }
-                    case "g" -> {
-                        System.out.println("yeongcheol");
-                        find = true;
-                    }
+                if (NAME_MAP.containsKey(token)) {
+                    System.out.println(NAME_MAP.get(token));
+                    return;
                 }
-
-                if (find) {
-                    break;
-                }
-            }
-
-            if (find) {
-                break;
             }
         }
     }
