fbc4227edf339406d891da5337ed77812f2ec7d7
thisisnew <thisisnew@naver.com>
2025-03-31 20:38:46 +0900

    20305번 커트라인

--- src/baekjoon/string/No25305.java
+++ src/baekjoon/string/No25305.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class No25305 {
@@ -19,13 +20,17 @@ public class No25305 {
 
         st = new StringTokenizer(br.readLine());
 
-        int[] arr = new int[n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int i = 0; i< n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+
+            pq.offer(Integer.parseInt(st.nextToken()));
+
+            if (pq.size() > k){
+                pq.poll();
+            }
         }
 
-        Arrays.sort(arr);
-        System.out.println(arr[n - k]);
+        System.out.println(pq.peek());
     }
 }
