645a98ca8a4e559b358e233bd8883b363f127cb3
thisisnew <thisisnew@naver.com>
2025-03-31 20:32:46 +0900

    20305번 커트라인

--- src/baekjoon/string/No25305.java
+++ src/baekjoon/string/No25305.java
@@ -3,6 +3,7 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.StringTokenizer;
 
@@ -11,17 +12,20 @@ public class No25305 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < 15; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            while (st.hasMoreTokens()) {
-                String token = st.nextToken();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-                if (NAME_MAP.containsKey(token)) {
-                    System.out.println(NAME_MAP.get(token));
-                    return;
-                }
-            }
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i< n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(arr);
+        System.out.println(arr[n - k]);
     }
 }
