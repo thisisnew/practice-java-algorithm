1fd96802d866106eda42d8b58b0fb8f695dbb30e
thisisnew <thisisnew@naver.com>
2025-04-01 21:16:31 +0900

    14215번 세 막대

--- src/baekjoon/impl/No14215.java
+++ src/baekjoon/impl/No14215.java
@@ -3,7 +3,6 @@ package baekjoon.impl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
