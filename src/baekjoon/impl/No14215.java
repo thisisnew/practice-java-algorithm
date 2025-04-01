46e3aa7f52807059982e2a63d5124bdaf33b66ce
thisisnew <thisisnew@naver.com>
2025-04-01 17:56:21 +0900

    14215번 세 막대

---
+++ src/baekjoon/impl/No14215.java
@@ -0,0 +1,32 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class No14215 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < 3; i++) {
+            pq.offer(Integer.parseInt(st.nextToken()));
+        }
+
+        int a = pq.poll();
+        int b = pq.poll();
+        int c = pq.poll();
+
+        while(c*c != a*b){
+            c--;
+        }
+
+        System.out.println(a+b+c);
+    }
+}
