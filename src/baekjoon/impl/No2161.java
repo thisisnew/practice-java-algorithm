52fd5e2657626167b23fedcf04c5ac4b96dfd11f
thisisnew <thisisnew@naver.com>
2025-04-02 20:34:10 +0900

    카드1

---
+++ src/baekjoon/impl/No2161.java
@@ -0,0 +1,23 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class No2161 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+
+    }
+}
