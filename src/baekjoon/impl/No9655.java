b2a4eda1c76b67ad1cb54784491e21f5229b4e48
thisisnew <thisisnew@naver.com>
2025-04-03 21:13:52 +0900

    9655번 돌 게임

---
+++ src/baekjoon/impl/No9655.java
@@ -0,0 +1,18 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class No9655 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println();
+    }
+}
