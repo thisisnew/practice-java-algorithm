1c4aec332bf0767197283a8055478543ebbd1c7e
thisisnew <thisisnew@naver.com>
2025-04-04 17:27:36 +0900

    9316번 Hello Judge

---
+++ src/baekjoon/impl/No9316.java
@@ -0,0 +1,18 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No9316 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Hello World, Judge " + i + "!");
+        }
+    }
+}
