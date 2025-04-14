95904c89505e4dbfa8cb152253320a72d8acd1df
thisisnew <thisisnew@naver.com>
2025-04-14 21:20:44 +0900

    1138번 한 줄로 서기

--- src/baekjoon/impl/No1138.java
+++ src/baekjoon/impl/No1138.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -15,11 +16,20 @@ public class No1138 {
         int n = Integer.parseInt(br.readLine());
 
         int[] arr = new int[n];
+        Arrays.fill(arr, -1);
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int count = 0;
+            int v = Integer.parseInt(st.nextToken());
+
+            if (arr[i] == -1) {
+                if (count == v) {
+                    arr[i] = i + 1;
+                }
+                count++;
+            }
         }
 
         System.out.println();
