55ecb2a9a1a819832056a1a5db04a089108e16db
thisisnew <thisisnew@naver.com>
2025-04-15 20:35:46 +0900

    15963번 CASIO

---
+++ src/baekjoon/impl/No15963.java
@@ -0,0 +1,23 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No15963 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+
+        int result = n == m ? 1 : 0;
+
+        System.out.println(result);
+    }
+}
