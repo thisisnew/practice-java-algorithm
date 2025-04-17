182010528c88e5e8e800d24360ed3ee51a303599
thisisnew <thisisnew@naver.com>
2025-04-17 20:12:33 +0900

    2587번 대표값2

--- src/baekjoon/impl/No2587.java
+++ src/baekjoon/impl/No2587.java
@@ -4,25 +4,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class No2587 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int sum = 0;
+            int[] arr = new int[5];
 
-        int sum = 0;
-        int[] arr = new int[5];
+            // 5개 정수 입력 받기
+            for (int i = 0; i < 5; i++) {
+                int n = Integer.parseInt(br.readLine());
+                sum += n;
+                arr[i] = n;
+            }
 
-        for (int i = 0; i < 5; i++) {
-            int n = Integer.parseInt(br.readLine());
-            sum += n;
-            arr[i] = n;
+            Arrays.sort(arr);
+
+            System.out.println(sum / 5);
+            System.out.println(arr[2]);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        Arrays.sort(arr);
-
-        System.out.println(sum/5);
-        System.out.println(arr[2]);
     }
 }
