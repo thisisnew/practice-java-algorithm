2500e018f1a2606144a505036f18f6bf968aceff
thisisnew <thisisnew@naver.com>
2025-04-18 21:07:36 +0900

    10996번 별 찍기 - 21

---
+++ src/baekjoon/impl/No10996.java
@@ -0,0 +1,18 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No10996 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+
+    }
+}
