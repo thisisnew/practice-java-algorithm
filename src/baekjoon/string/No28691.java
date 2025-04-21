b88a62e9d636ed92f46e0082f6f7cff0c040eae1
thisisnew <thisisnew@naver.com>
2025-04-21 18:25:05 +0900

    28691번 정보보호학부 동아리 소개

--- src/baekjoon/string/No28691.java
+++ src/baekjoon/string/No28691.java
@@ -3,21 +3,21 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
 
 public class No28691 {
-    private static final Map<String, String> CLUB_MAP = Map.of(
-        "M", "MatKor",
-        "W", "WiCys",
-        "C", "CyKor",
-        "A", "AlKor",
-        "$", "$clear"
-    );
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
+        char input = br.readLine().charAt(0);
+        
+        String result = switch (input) {
+            case 'M' -> "MatKor";
+            case 'W' -> "WiCys";
+            case 'C' -> "CyKor";
+            case 'A' -> "AlKor";
+            case '$' -> "$clear";
+            default -> throw new IllegalArgumentException();
+        };
 
-        System.out.println(CLUB_MAP.get(input));
+        System.out.println(result);
     }
 }
