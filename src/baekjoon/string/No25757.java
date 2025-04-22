dc882396704f0ba790fe40d64ef54f6068b6f2bb
thisisnew <thisisnew@naver.com>
2025-04-22 19:13:46 +0900

    25757번 임스와 함께하는 미니게임

---
+++ src/baekjoon/string/No25757.java
@@ -0,0 +1,20 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No25757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        String game = st.nextToken();
+
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+}
