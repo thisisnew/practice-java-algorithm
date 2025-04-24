7f537c3a2452fb24559ea7fd70feb3fe828c35e5
thisisnew <thisisnew@naver.com>
2025-04-24 18:40:15 +0900

    10170번 NFC West vs North

--- src/baekjoon/string/No10170.java
+++ src/baekjoon/string/No10170.java
@@ -2,18 +2,22 @@ package baekjoon.string;
 
 public class No10170 {
     public static void main(String[] args) {
-        System.out.println("NFC West       W   L  T");
-        System.out.println("-----------------------");
-        System.out.println("Seattle        13  3  0");
-        System.out.println("San Francisco  12  4  0");
-        System.out.println("Arizona        10  6  0");
-        System.out.println("St. Louis      7   9  0");
-        System.out.println();
-        System.out.println("NFC North      W   L  T");
-        System.out.println("-----------------------");
-        System.out.println("Green Bay      8   7  1");
-        System.out.println("Chicago        8   8  0");
-        System.out.println("Detroit        7   9  0");
-        System.out.println("Minnesota      5  10  1");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("NFC West       W   L  T\n");
+        sb.append("-----------------------\n");
+        sb.append("Seattle        13  3  0\n");
+        sb.append("San Francisco  12  4  0\n");
+        sb.append("Arizona        10  6  0\n");
+        sb.append("St. Louis      7   9  0\n");
+        sb.append("\n");
+        sb.append("NFC North      W   L  T\n");
+        sb.append("-----------------------\n");
+        sb.append("Green Bay      8   7  1\n");
+        sb.append("Chicago        8   8  0\n");
+        sb.append("Detroit        7   9  0\n");
+        sb.append("Minnesota      5  10  1");
+
+        System.out.print(sb);
     }
 }
