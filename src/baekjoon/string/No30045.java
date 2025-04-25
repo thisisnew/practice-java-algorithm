1f091a4ad43b7647bd171032fdce8647facbcb20
thisisnew <thisisnew@naver.com>
2025-04-25 16:15:49 +0900

    30045번 ZOAC 6

---
+++ src/baekjoon/string/No30045.java
@@ -0,0 +1,20 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No30045 {
+    public static void main(String[] args) {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+
+        }
+
+        br.close();
+    }
+}
