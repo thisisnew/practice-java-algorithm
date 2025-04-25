f6b15b8bc86218eb8854389290a17d20c64fb8b6
thisisnew <thisisnew@naver.com>
2025-04-25 16:18:11 +0900

    30045번 ZOAC 6

--- src/baekjoon/string/No30045.java
+++ src/baekjoon/string/No30045.java
@@ -1,20 +1,32 @@
 package baekjoon.string;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class No30045 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
+        int result = 0;
+
         for (int i = 0; i < n; i++) {
-
-
+            String s = br.readLine();
+            result += count(s);
         }
 
+        System.out.println(result);
         br.close();
+    }
+
+    private static int count(String s) {
+        if (s.length() < 2) {
+            return 0;
+        }
+
+
     }
 }
