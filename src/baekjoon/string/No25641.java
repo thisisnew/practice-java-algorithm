0fec1a90d24d9cb2c60af0bbb91ee03092d28ad2
thisisnew <thisisnew@naver.com>
2025-04-27 19:08:56 +0900

    25641번 균형 잡힌 소떡소떡

---
+++ src/baekjoon/string/No25641.java
@@ -0,0 +1,53 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class No25641 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sCnt = 0;
+        int tCnt = 0;
+
+        int n = Integer.parseInt(br.readLine());
+
+        char[] chars = br.readLine().toCharArray();
+        Queue<Character> q = new LinkedList<>();
+
+        for (char c : chars) {
+
+            if (c == 's') {
+                sCnt++;
+            }
+
+            if (c == 't') {
+                tCnt++;
+            }
+
+            q.add(c);
+        }
+
+        while(tCnt != sCnt) {
+            char c = q.poll();
+
+            if (c == 's') {
+                sCnt--;
+            }
+
+            if (c == 't') {
+                tCnt--;
+            }
+        }
+
+        for (char c : q) {
+            System.out.print(c);
+        }
+
+        br.close();
+    }
+}
