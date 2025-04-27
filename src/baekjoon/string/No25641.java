116b2bd1dfb7ca5e8d2fe12ed2edb6c13cf9f9f4
thisisnew <thisisnew@naver.com>
2025-04-27 19:22:34 +0900

    25641번 균형 잡힌 소떡소떡

--- src/baekjoon/string/No25641.java
+++ src/baekjoon/string/No25641.java
@@ -3,48 +3,42 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class No25641 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int sCnt = 0;
-        int tCnt = 0;
-
+        
         int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
 
-        char[] chars = br.readLine().toCharArray();
-        Queue<Character> q = new LinkedList<>();
-
-        for (char c : chars) {
-            if (c == 's') {
-                sCnt++;
+        int sCount = 0;
+        int tCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (input.charAt(i) == 's') {
+                sCount++;
             }
 
-            if (c == 't') {
-                tCnt++;
-            }
-
-            q.add(c);
-        }
-
-        while(tCnt != sCnt) {
-            char c = Objects.requireNonNull(q.poll());
-
-            if (c == 's') {
-                sCnt--;
-            }
-
-            if (c == 't') {
-                tCnt--;
+            if (input.charAt(i) == 't') {
+                tCount++;
             }
         }
 
-        for (char c : q) {
-            System.out.print(c);
+        int idx = 0;
+        while (sCount != tCount) {
+            char removed = input.charAt(idx);
+            if (removed == 's') {
+                sCount--;
+            }
+
+            if (removed == 't') {
+                tCount--;
+            }
+
+            idx++;
         }
 
+        System.out.println(input.substring(idx));
+        
         br.close();
     }
 }
