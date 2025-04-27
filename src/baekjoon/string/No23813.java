3e107a1687daf2c5e0dd2582345da4a31ac85ea9
thisisnew <thisisnew@naver.com>
2025-04-28 00:00:06 +0900

    23813번 회전

--- src/baekjoon/string/No23813.java
+++ src/baekjoon/string/No23813.java
@@ -9,20 +9,24 @@ public class No23813 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = br.readLine();
+        int n = Integer.parseInt(br.readLine());
 
         List<Integer> list = new ArrayList<>();
 
-        for (int i = input.length() - 1; i > 0; i--) {
+        int v = 10;
 
-            String front = input.substring(0, i);
-            String back = input.substring(i);
+        while (n%v != n) {
+            int front = n/v;
+            int back = n%v;
 
-            list.add(Integer.parseInt(back + front));
+            String s = back + String.valueOf(front);
+            list.add(Integer.parseInt(s));
+
+            v *= 10;
         }
 
-        System.out.println(list);
-
+        int result = list.stream().mapToInt(i -> i).sum();
+        System.out.println(result + n);
         br.close();
     }
 }
