b4e1e15979a1db347f951665cd277230f6d4d542
thisisnew <thisisnew@naver.com>
2025-04-29 00:21:51 +0900

    20114번 미아 노트

---
+++ src/baekjoon/string/No20114.java
@@ -0,0 +1,21 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class No20114 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+
+        br.close();
+    }
+}
