15bf7f165bb226da1c9bb85c918bf6a5d54691e5
thisisnew <thisisnew@naver.com>
2025-05-03 08:45:46 +0900

    28445번 알록달록 앵무새

---
+++ src/baekjoon/string/No28445.java
@@ -0,0 +1,24 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No28445 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String fBodyColor = st.nextToken();
+        String fTailColor = st.nextToken();
+
+        st = new StringTokenizer(br.readLine());
+        String mBodyColor = st.nextToken();
+        String mTailColor = st.nextToken();
+
+
+        br.close();
+    }
+}
