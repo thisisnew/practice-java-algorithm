649059ca7122b9a34576a710b706adf219e45ecb
thisisnew <thisisnew@naver.com>
2025-05-02 16:46:33 +0900

    2460번 지능형 기차 2

---
+++ src/baekjoon/impl/No2460.java
@@ -0,0 +1,28 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class No2460 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int current = 0;
+        int max = 0;
+        for (int i=0; i<10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            current -= Integer.parseInt(st.nextToken());
+            current += Integer.parseInt(st.nextToken());
+
+            if (current > max) {
+                max = current;
+            }
+        }
+
+        System.out.println(max);
+        br.close();
+    }
+}
