8aaef8462a816a1bbbc3c7a0eb6a88b959c0a3bd
thisisnew <thisisnew@naver.com>
2025-05-04 00:29:46 +0900

    25250번 경로당펑크 2077

---
+++ src/baekjoon/string/No25250.java
@@ -0,0 +1,31 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No25250 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char[] s = br.readLine().toCharArray();
+
+        char[] arr = {'r', 's', 'e', 'f', 'a', 'q', 't', 'd', 'w', 'c', 'z', 'x', 'v', 'g'};
+
+        boolean find = false;
+        char last = s[n-1];
+        for (char c : arr) {
+            if (c == last) {
+                System.out.println(1);
+                find = true;
+                break;
+            }
+        }
+
+        if (!find) {
+            System.out.println(0);
+        }
+
+        br.close();
+    }
+}
