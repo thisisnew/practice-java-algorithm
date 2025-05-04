d54dcca65c5f9fd6f8f0413a5a0965daac9061e1
thisisnew <thisisnew@naver.com>
2025-05-04 19:32:47 +0900

    27494번 2023년은 검은 토끼의 해

--- src/baekjoon/string/No27494.java
+++ src/baekjoon/string/No27494.java
@@ -8,28 +8,28 @@ public class No27494 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr_2023 = { 2, 0, 2, 3 };
+        char[] arr_2023 = { '2', '0', '2', '3' };
 
         if (n < 2023) {
             System.out.println(0);
         } else {
             int result  = 0;
 
-            loop: for (int i = 2023; i < n; i++) {
-                int idx = 3;
-                int div = 10;
+            for (int i = 2023; i < n; i++) {
+                String value = String.valueOf(i);
+                int idx = 0;
 
-                while (i / div > 0) {
-                    if (i % div == arr_2023[idx]) {
-                        idx--;
+                for (int j = 0; j < value.length(); j++) {
+                    char c = value.charAt(j);
+
+                    if (c == arr_2023[idx]) {
+                        idx++;
+
+                        if (idx == 4) {
+                            result++;
+                            break;
+                        }
                     }
-
-                    if (idx == -1) {
-                        result++;
-                        break loop;
-                    }
-
-                    div *= 10;
                 }
             }
 
