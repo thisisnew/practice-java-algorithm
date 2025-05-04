01ee4a5318de02b919ba3e234024ae400d54cbdd
thisisnew <thisisnew@naver.com>
2025-05-04 19:38:09 +0900

    27494번 2023년은 검은 토끼의 해

--- src/baekjoon/string/No27494.java
+++ src/baekjoon/string/No27494.java
@@ -5,10 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class No27494 {
+
+    private static final char[] ARR_2023 = { '2', '0', '2', '3' };
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        char[] arr_2023 = { '2', '0', '2', '3' };
 
         if (n < 2023) {
             System.out.println(0);
@@ -16,20 +18,8 @@ public class No27494 {
             int result  = 0;
 
             for (int i = 2023; i < n; i++) {
-                String value = String.valueOf(i);
-                int idx = 0;
-
-                for (int j = 0; j < value.length(); j++) {
-                    char c = value.charAt(j);
-
-                    if (c == arr_2023[idx]) {
-                        idx++;
-
-                        if (idx == 4) {
-                            result++;
-                            break;
-                        }
-                    }
+                if (matches(i)) {
+                    result++;
                 }
             }
 
@@ -37,5 +27,22 @@ public class No27494 {
         }
 
         br.close();
+    }
+
+    private static boolean matches(int num) {
+        char[] values = String.valueOf(num).toCharArray();
+        int idx = 0;
+
+        for (char c : values) {
+            if (c == ARR_2023[idx]) {
+                idx++;
+
+                if (idx == 4) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
