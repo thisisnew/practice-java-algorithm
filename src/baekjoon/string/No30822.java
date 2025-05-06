550f25244e4764925720effab297bf9ee9f07c26
thisisnew <thisisnew@naver.com>
2025-05-06 10:54:41 +0900

    30822번 UOSPC 세기

--- src/baekjoon/string/No30822.java
+++ src/baekjoon/string/No30822.java
@@ -12,29 +12,27 @@ public class No30822 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String input = br.readLine();
 
-        Map<String, Integer> charMap = new HashMap<>();
+        Map<Character, Integer> charMap = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            String input = st.nextToken();
+            char c = input.charAt(i);
+            if (!matches(c)) continue;
 
-            if (!matches(input)) continue;
-
-            int count = charMap.getOrDefault(input, 0);
-            charMap.put(input, ++count);
+            int count = charMap.getOrDefault(c, 0);
+            charMap.put(c, ++count);
         }
 
         int min = 1000;
-        for (Map.Entry<String, Integer> entry : charMap.entrySet()) {
+        for (Map.Entry<Character, Integer> entry : charMap.entrySet()) {
             if (entry.getValue() < min) min = entry.getValue();
         }
         System.out.println(min);
         br.close();
     }
 
-    private static boolean matches(String input) {
-        char c = input.charAt(0);
+    private static boolean matches(char c) {
         return c == 'u' || c == 'o' || c == 's' || c == 'p' || c == 'c';
     }
 }
