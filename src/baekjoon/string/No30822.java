59649a9645e7453e2102cc68b2ab4aab3402372e
thisisnew <thisisnew@naver.com>
2025-05-06 10:20:53 +0900

    30822번 UOSPC 세기

---
+++ src/baekjoon/string/No30822.java
@@ -0,0 +1,30 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class No30822 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Map<String, Integer> charMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = st.nextToken();
+
+
+
+            int count = charMap.getOrDefault(input, 0);
+            charMap.put(input, ++count);
+        }
+
+        br.close();
+    }
+}
