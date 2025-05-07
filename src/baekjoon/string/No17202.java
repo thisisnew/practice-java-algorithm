a44142ee71c10882f5ec942c6d013dfc045cfabb
thisisnew <thisisnew@naver.com>
2025-05-07 18:37:27 +0900

    17202번 핸드폰 번호 궁합

---
+++ src/baekjoon/string/No17202.java
@@ -0,0 +1,19 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class No17202 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+
+        System.out.println(names.get(0));
+        br.close();
+    }
+}
