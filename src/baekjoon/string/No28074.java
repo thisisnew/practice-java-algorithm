19f2cc90d99bcc205510e7ad3cffc409026c0568
thisisnew <thisisnew@naver.com>
2025-05-11 08:31:56 +0900

    28074번 모비스

---
+++ src/baekjoon/string/No28074.java
@@ -0,0 +1,29 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class No28074 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = 0;
+        Map<Character, Boolean> map = new HashMap<>();
+
+        String input = br.readLine();
+
+        for (char c : input.toCharArray()) {
+
+
+
+
+        }
+
+
+
+        br.close();
+    }
+}
