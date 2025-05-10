0a2c8847f4223c81c13fead819b3c9072c081071
thisisnew <thisisnew@naver.com>
2025-05-11 08:09:29 +0900

    119432번 파일 옮기기

---
+++ src/baekjoon/impl/No11943.java
@@ -0,0 +1,28 @@
+package baekjoon.impl;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No11943 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+
+
+
+        br.close();
+    }
+}
