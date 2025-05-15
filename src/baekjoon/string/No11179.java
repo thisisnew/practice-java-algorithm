fad7be6a75794c51eb61766c6ec32fb75a8e3af9
thisisnew <thisisnew@naver.com>
2025-05-15 21:22:52 +0900

    11179번 2진수 뒤집기

--- src/baekjoon/string/No11179.java
+++ src/baekjoon/string/No11179.java
@@ -3,33 +3,29 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class No11179 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Queue<Integer> queue = new LinkedList<>();
+        Stack<Integer> stack = new Stack<>();
         int n = Integer.parseInt(br.readLine());
 
         while (n > 0) {
-            queue.add(n % 2);
+            stack.push(n % 2);
             n /= 2;
         }
 
         int result = 0;
         int i = 0;
-        while (!queue.isEmpty()) {
-            result += (int) Math.pow(2, i) * queue.poll();
+        while (!stack.isEmpty()) {
+            result += (int) Math.pow(2, i) * stack.pop();
             i++;
         }
 
         System.out.println(result);
         br.close();
     }
-
 }
