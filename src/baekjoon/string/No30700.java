193f4072a5476dc80f52909f1ac0df0c1aad5ea3
thisisnew <thisisnew@naver.com>
2025-05-17 07:15:25 +0900

    30700번 KOREA 문자열 만들기

--- src/baekjoon/string/No30700.java
+++ src/baekjoon/string/No30700.java
@@ -10,32 +10,23 @@ public class No30700 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        char[] arr = { 'K', 'O', 'R', 'E', 'A' };
+        int idx = 0;
         String input = br.readLine();
-        Map<Character, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
 
         for (char c : input.toCharArray()) {
-            if (c == 'K' || c == 'O' || c == 'R' || c == 'E' || c == 'A') {
-                map.put(c, map.getOrDefault(c, 0) + 1);
+            if (c == arr[idx]) {
+                idx++;
+                sb.append(c);
+            }
+
+            if (idx == arr.length) {
+                idx = 0;
             }
         }
 
-        if (map.size() < 5) {
-            System.out.println(0);
-        } else {
-            int result = 1000;
-
-            for (int v : map.values()) {
-                if (v > result) {
-                    continue;
-                }
-
-                result = v;
-            }
-
-            System.out.println(result);
-        }
-
+        System.out.println(sb.toString().length());
         br.close();
     }
 }
