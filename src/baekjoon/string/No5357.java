a992cc53933d38b10e39570786a116fa8a9c314f
thisisnew <thisisnew@naver.com>
2025-05-20 20:24:40 +0900

    5357번 Dedupe

--- src/baekjoon/string/No5357.java
+++ src/baekjoon/string/No5357.java
@@ -3,6 +3,7 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.Set;
 
 public class No5357 {
@@ -16,6 +17,20 @@ public class No5357 {
 
         for(int i = 0; i < n; i++) {
             String input = br.readLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char c : input.toCharArray()) {
+                if (sb.isEmpty()) {
+                    sb.append(c);
+                }
+
+                if (c != sb.charAt(sb.length() - 1)) {
+                    sb.append(c);
+                }
+            }
+
+            System.out.println(sb);
         }
 
         br.close();
