8fd485584e2abbc80f5f40a55d96171eac0a741a
thisisnew <thisisnew@naver.com>
2025-05-20 20:26:38 +0900

    5357번 Dedupe

--- src/baekjoon/string/No5357.java
+++ src/baekjoon/string/No5357.java
@@ -8,24 +8,18 @@ import java.util.Set;
 
 public class No5357 {
 
-    private static final Set<String> PLAYING_STATIONS = Set.of("PS4", "PS5");
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine().trim());
 
         for(int i = 0; i < n; i++) {
             String input = br.readLine();
-
             StringBuilder sb = new StringBuilder();
 
             for (char c : input.toCharArray()) {
                 if (sb.isEmpty()) {
                     sb.append(c);
-                }
-
-                if (c != sb.charAt(sb.length() - 1)) {
+                } else if (c != sb.charAt(sb.length() - 1)) {
                     sb.append(c);
                 }
             }
