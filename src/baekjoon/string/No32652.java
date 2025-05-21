abe08bef3c2be98e33dae5ce444956fa125acccc
thisisnew <thisisnew@naver.com>
2025-05-21 18:55:51 +0900

    32652번 아카라카 2

--- src/baekjoon/string/No32652.java
+++ src/baekjoon/string/No32652.java
@@ -5,13 +5,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class No32652 {
-
-    private static final char[] UOS_CHARS = new char[]{'U', 'O', 'S'};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int k = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<k; i++) {
+
+            if (i == 0 ) {
+                sb.append("AKARAKA");
+                continue;
+            }
+
+            sb.append("RAKA");
+        }
+
+        sb.deleteCharAt(sb);
         br.close();
     }
 
