e75271be90341c5d707d5e812c51a3e2609333a2
thisisnew <thisisnew@naver.com>
2025-05-21 18:23:58 +0900

    32652번 아카라카 2

---
+++ src/baekjoon/string/No32652.java
@@ -0,0 +1,18 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No32652 {
+
+    private static final char[] UOS_CHARS = new char[]{'U', 'O', 'S'};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+
+        br.close();
+    }
+
+}
