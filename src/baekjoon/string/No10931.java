8f2c4c791f9ebaae08293529aae76f5f0a0cee70
thisisnew <thisisnew@naver.com>
2025-05-22 21:41:07 +0900

    10931번 SHA-384

--- src/baekjoon/string/No10931.java
+++ src/baekjoon/string/No10931.java
@@ -7,24 +7,25 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class No10931 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
 
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-384");
-            byte[] hash = digest.digest(s.getBytes());
-            StringBuilder hexString = new StringBuilder();
-            for (byte b : hash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
+        MessageDigest digest = MessageDigest.getInstance("SHA-384");
+        byte[] hash = digest.digest(s.getBytes());
+        StringBuilder sb = new StringBuilder();
+
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+
+            if (hex.length() == 1) {
+                sb.append('0');
             }
-            System.out.println(hexString);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
+
+            sb.append(hex);
         }
 
+        System.out.println(sb);
         br.close();
     }
 
