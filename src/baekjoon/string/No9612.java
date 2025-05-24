8dab2e0f67b54cc97151e7b63cb404c3357c0731
thisisnew <thisisnew@naver.com>
2025-05-24 22:49:29 +0900

    9612번 Maximum Word Frequency

---
+++ src/baekjoon/string/No9612.java
@@ -0,0 +1,25 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class No9612 {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+
+
+
+        }
+
+        br.close();
+    }
+
+}
