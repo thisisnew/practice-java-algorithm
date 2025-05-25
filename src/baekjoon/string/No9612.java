fdb7a7efdba396859c9d9b40e2066a0fa47256ec
thisisnew <thisisnew@naver.com>
2025-05-25 09:21:31 +0900

    9612번 Maximum Word Frequency

--- src/baekjoon/string/No9612.java
+++ src/baekjoon/string/No9612.java
@@ -3,18 +3,16 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class No9612 {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int max = 0;
+        int maxCount = 0;
+        String result = "";
         Map<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
@@ -22,18 +20,13 @@ public class No9612 {
             int count = map.getOrDefault(letter, 0) + 1;
             map.put(letter, count);
 
-            max = Math.max(max, count);
+            if (count > maxCount || (count == maxCount && letter.compareTo(result) > 0)) {
+                maxCount = count;
+                result = letter;
+            }
         }
 
-        final int maxValue = max;
-        List<String> letters = map.entrySet().stream()
-                .filter(entry -> entry.getValue() >= maxValue)
-                .map(Map.Entry::getKey)
-                .sorted()
-                .toList();
-
-        String result = letters.get(letters.size() - 1);
-        System.out.println(result + " " + map.get(result));
+        System.out.println(result + " " + maxCount);
         br.close();
     }
 
