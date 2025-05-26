1f0b553b0ccd29b80007a45c95927e79b8aec94d
thisisnew <thisisnew@naver.com>
2025-05-26 18:39:02 +0900

    29713번 브실이의 띠부띠부씰 컬렉션

---
+++ src/baekjoon/string/No29713.java
@@ -0,0 +1,34 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class No29713 {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int result = 0;
+        Map<Character, Integer> alphabet = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            alphabet.put(s.charAt(i), alphabet.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        int idx = 0;
+        while (alphabet.get(s.charAt(idx++)) != 0) {
+            if (idx == s.length()) {
+                result++;
+                idx = 0;
+            }
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
