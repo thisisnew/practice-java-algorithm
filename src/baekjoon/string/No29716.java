96180f07ddddb01c7ae3c5823ef614551fc084cf
thisisnew <thisisnew@naver.com>
2025-05-27 18:49:27 +0900

    29716번 풀만한문제

--- src/baekjoon/string/No29716.java
+++ src/baekjoon/string/No29716.java
@@ -3,29 +3,22 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class No29716 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] input = br.readLine().split(" ");
 
-        int j = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
 
         int result = 0;
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+            int size = calculate(br.readLine());
 
-            int v = st.countTokens()-1;
-            while (st.hasMoreTokens()) {
-                v += calculate(st.nextToken());
-            }
-
-            if (v <= j) {
+            if (size <= j) {
                 result++;
             }
         }
@@ -36,9 +29,21 @@ public class No29716 {
 
     private static int calculate(String s) {
         int sum = 0;
+
         for (char c : s.toCharArray()) {
-            sum += Character.isUpperCase(c) ? 4 : 2;
+            if (Character.isUpperCase(c)) {
+                sum += 4;
+            }
+
+            if (Character.isLowerCase(c) || Character.isDigit(c)) {
+                sum += 2;
+            }
+
+            if (c == ' ') {
+                sum += 1;
+            }
         }
+
         return sum;
     }
 }
