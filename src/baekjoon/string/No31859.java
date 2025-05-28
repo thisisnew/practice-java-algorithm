1d9fdf0ccc161ec3f4ce50b5813247c6a02e491b
thisisnew <thisisnew@naver.com>
2025-05-28 20:39:09 +0900

    31859번 SMUPC NAME

--- src/baekjoon/string/No31859.java
+++ src/baekjoon/string/No31859.java
@@ -11,8 +11,7 @@ public class No31859 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-        String s = br.readLine();
+        String s = removeDuplicates(br.readLine());
 
 
         br.close();
