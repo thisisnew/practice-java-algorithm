3ddd12a240b22b98aa7b050f612a6c13bfe10839
thisisnew <thisisnew@naver.com>
2025-05-28 20:27:23 +0900

    1907번 탄소 화합물

--- src/baekjoon/string/No1907.java
+++ src/baekjoon/string/No1907.java
@@ -10,7 +10,17 @@ public class No1907 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String s = br.readLine();
+        String[] fSplit = splitStr(s, "+");
+        String m1 = splitStr(s, "+")[0];
+        String[] sSplit = splitStr(fSplit[1], "=");
+        String m2 = sSplit[0];
+        String m3 = sSplit[1];
+
 
         br.close();
+    }
+
+    private static String[] splitStr(String s, String separator) {
+        return s.split(separator);
     }
 }
