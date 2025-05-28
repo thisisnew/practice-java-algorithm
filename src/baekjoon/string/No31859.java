13160151e4e78a7f23dfb346327d3c2b0f1712bf
thisisnew <thisisnew@naver.com>
2025-05-28 20:37:36 +0900

    31859번 SMUPC NAME

---
+++ src/baekjoon/string/No31859.java
@@ -0,0 +1,35 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class No31859 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        String s = br.readLine();
+
+
+        br.close();
+    }
+
+    private static String removeDuplicates(String s) {
+
+        Set<Character> charSet = new HashSet<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (!charSet.contains(c)) {
+                charSet.add(c);
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
