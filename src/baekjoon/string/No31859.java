a7ab644f5ca59f26e21a568e990f205a5dbe0768
thisisnew <thisisnew@naver.com>
2025-05-30 18:02:33 +0900

    31859번 SMUPC NAME

--- src/baekjoon/string/No31859.java
+++ src/baekjoon/string/No31859.java
@@ -11,58 +11,24 @@ public class No31859 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         String s = st.nextToken();
 
-        String st1Result = step1(s);
-        String st2Result = step2(s, st1Result);
-        String st3Result = step3(n, st2Result);
-        String st4Result = step4(st3Result);
-        String st5Result = step5(st4Result);
-
-        System.out.println(st5Result);
-        br.close();
-    }
-
-    private static String step1(String s) {
-
-        Set<Character> charSet = new HashSet<>();
-        StringBuilder sb = new StringBuilder();
-
+        Set<Character> seen = new HashSet<>();
+        StringBuilder unique = new StringBuilder();
         for (char c : s.toCharArray()) {
-            if (!charSet.contains(c)) {
-                charSet.add(c);
-                sb.append(c);
-            }
+            if (seen.add(c)) unique.append(c);
         }
 
-        return sb.toString();
-    }
+        String result = "smupc_" + new StringBuilder()
+                .append(1906 + n)
+                .append(unique)
+                .append(4 + s.length() - unique.length())
+                .reverse();
 
-    private static String step2(String before, String after) {
-        int v = 4 + before.length() - after.length();
-        return String.format("%s%d", after, v);
-    }
-
-    private static String step3(int n, String s) {
-        int v = 1906 + n;
-        return String.format("%d%s", v, s);
-    }
-
-    private static String step4(String s) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            sb.append(s.charAt(i));
-        }
-
-        return sb.toString();
-    }
-
-    private static String step5(String s) {
-        return "smupc_" + s;
+        System.out.println(result);
+        br.close();
     }
 }
