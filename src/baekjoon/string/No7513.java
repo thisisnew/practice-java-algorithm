6773308630d6c04953926ade5af1fd4bd33197e0
thisisnew <thisisnew@naver.com>
2025-05-31 01:50:51 +0900

    7513번 준살 프로그래밍 대회

---
+++ src/baekjoon/string/No7513.java
@@ -0,0 +1,31 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class No7513 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int m = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < m; j++) {
+                String word = br.readLine();
+            }
+
+            int n = Integer.parseInt(br.readLine());
+
+
+        }
+        
+        br.close();
+    }
+}
