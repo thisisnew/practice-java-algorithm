c2142ca0db2a5c7477619f5c0e23168d1021ea49
thisisnew <thisisnew@naver.com>
2025-06-02 06:16:11 +0900

    28064번 이민희진

---
+++ src/baekjoon/string/No28064.java
@@ -0,0 +1,23 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class No28064 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+
+
+
+        }
+
+        br.close();
+    }
+}
