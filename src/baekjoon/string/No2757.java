094e5a6305250176076bc3adab58c0ee842fb565
thisisnew <thisisnew@naver.com>
2025-06-01 15:38:45 +0900

    2757번 엑셀

---
+++ src/baekjoon/string/No2757.java
@@ -0,0 +1,40 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class No2757 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String s = br.readLine();
+
+            if (s.equals("R0C0")) {
+                break;
+            }
+
+            Map<String, Integer> rcMap = createRcMap(s);
+
+
+        }
+
+        br.close();
+    }
+
+    private static Map<String, Integer> createRcMap(String s) {
+        String[] split = s.split("C");
+        String s1 = split[0];
+        String s2 = split[1];
+
+        Map<String, Integer> result = new HashMap<>();
+        result.put("R", Integer.parseInt(s1));
+        result.put("C", Integer.parseInt(s2));
+
+        return result;
+    }
+}
