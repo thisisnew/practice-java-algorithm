092cd5de0ab16fef9b01778eda404625c9044c68
thisisnew <thisisnew@naver.com>
2025-06-07 08:55:15 +0900

    33709번 치매예방수칙 3.3.3

--- src/baekjoon/string/No33709.java
+++ src/baekjoon/string/No33709.java
@@ -3,9 +3,6 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Objects;
-import java.util.Queue;
 
 public class No33709 {
 
@@ -30,7 +27,7 @@ public class No33709 {
             sb = new StringBuilder();
         }
 
-        if (!sb.isEmpty()) {
+        if (sb.length() > 0) {
             res += Integer.parseInt(sb.toString());
         }
 
