17cfb8c0f858b7ec44e3da50d7ad259ea162062e
thisisnew <thisisnew@naver.com>
2025-06-06 20:36:48 +0900

    33709번 치매예방수칙 3.3.3

---
+++ src/baekjoon/string/No33709.java
@@ -0,0 +1,21 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
+public class No33709 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+
+        br.close();
+    }
+}
