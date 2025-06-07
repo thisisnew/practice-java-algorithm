e37a17c2c65989512d192eb7746d62956ba772af
thisisnew <thisisnew@naver.com>
2025-06-08 00:27:00 +0900

    33845번 PNUPC에 한 번도 빠지지 않고 출연한 산지니가 새삼 대단하다고 느껴지네

---
+++ src/baekjoon/string/No33845.java
@@ -0,0 +1,17 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No33845 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String t = br.readLine();
+
+        br.close();
+    }
+}
