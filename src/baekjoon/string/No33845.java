01edce53ad9b8bd48113602e3f1c7a9cb9aa10ad
thisisnew <thisisnew@naver.com>
2025-06-08 00:32:54 +0900

    33845번 PNUPC에 한 번도 빠지지 않고 출연한 산지니가 새삼 대단하다고 느껴지네

--- src/baekjoon/string/No33845.java
+++ src/baekjoon/string/No33845.java
@@ -3,6 +3,8 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class No33845 {
 
@@ -11,6 +13,19 @@ public class No33845 {
 
         String s = br.readLine();
         String t = br.readLine();
+
+        Set<Character> sSet = s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(HashSet::new, Set::add, Set::addAll);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : t.toCharArray()) {
+            if (!sSet.contains(c)) {
+                sb.append(c);
+            }
+        }
+
 
         br.close();
     }
