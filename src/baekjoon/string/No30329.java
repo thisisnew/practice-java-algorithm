e1d35b48cf8f6113abb8aff4fe3ff1150bd3f1a6
thisisnew <thisisnew@naver.com>
2025-06-13 19:34:20 +0900

    30329번 Kick

---
+++ src/baekjoon/string/No30329.java
@@ -0,0 +1,15 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No30329 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        br.close();
+    }
+}
