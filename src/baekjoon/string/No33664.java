aea58fd2544801c07f1ac64b21bd1382a23f6998
thisisnew <thisisnew@naver.com>
2025-06-14 23:42:42 +0900

    33664번 현실적인 생일 축하 방안

---
+++ src/baekjoon/string/No33664.java
@@ -0,0 +1,20 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No33664 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int b = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        br.close();
+    }
+}
