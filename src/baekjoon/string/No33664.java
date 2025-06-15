4fc1de650719a9430d784f7d2b2e30f1be934fb8
thisisnew <thisisnew@naver.com>
2025-06-15 09:34:08 +0900

    33664번 현실적인 생일 축하 방안

--- src/baekjoon/string/No33664.java
+++ src/baekjoon/string/No33664.java
@@ -3,6 +3,8 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class No33664 {
@@ -14,6 +16,14 @@ public class No33664 {
         int b = Integer.parseInt(st.nextToken()); // 토카의 보유 금액
         int n = Integer.parseInt(st.nextToken()); // 아이템의 수
         int m = Integer.parseInt(st.nextToken()); // 구매하려는 아이템의 수
+
+        Map<String, Integer> items = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            items.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+        }
+
+
 
         br.close();
     }
