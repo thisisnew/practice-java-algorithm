0e81983e2809d9280d2cc8e394b7d78c486e2b16
thisisnew <thisisnew@naver.com>
2025-06-16 18:00:16 +0900

    32401번 ANA는 회문이야

---
+++ src/baekjoon/string/No32401.java
@@ -0,0 +1,20 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class No32401 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        br.close();
+    }
+}
