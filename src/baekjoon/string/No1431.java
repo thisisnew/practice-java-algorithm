e26210342f419f1878a51a4110fa96e25d7c174a
thisisnew <thisisnew@naver.com>
2025-06-19 23:57:25 +0900

    1431번 시리얼 번호

---
+++ src/baekjoon/string/No1431.java
@@ -0,0 +1,31 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class No1431 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr);
+
+        for (String s : arr) {
+            System.out.println(s);
+        }
+
+        br.close();
+    }
+}
