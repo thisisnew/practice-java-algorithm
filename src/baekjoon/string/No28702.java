a4c339e7df4874cbfbbfc025a9982ab1a2aeadf3
thisisnew <thisisnew@naver.com>
2025-06-22 00:50:45 +0900

    28702번 FizzBuzz

---
+++ src/baekjoon/string/No28702.java
@@ -0,0 +1,22 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class No28702 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+        String c = br.readLine();
+
+        br.close();
+    }
+}
