1e08e48b60471ca3c280a8c92435c97bab3334cd
thisisnew <thisisnew@naver.com>
2025-06-21 19:22:32 +0900

    31403번 A+B-C

--- src/baekjoon/string/No31403.java
+++ src/baekjoon/string/No31403.java
@@ -16,14 +16,16 @@ public class No31403 {
         String b = br.readLine();
         String c = br.readLine();
 
+        System.out.println(calAsInt(a, b, c));
+        System.out.println(calAsString(a, b, c));
         br.close();
     }
 
-    private int cal(String a, String b, String c) {
+    private static int calAsInt(String a, String b, String c) {
         return Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c);
     }
 
-    private int cal(int a, int b, int c) {
-        return a + b - c;
+    private static int calAsString(String a, String b, String c) {
+        return Integer.parseInt(a + b) - Integer.parseInt(c);
     }
 }
