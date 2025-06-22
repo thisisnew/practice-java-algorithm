00e0214cee642a4dbc1c995e8f012e60be192ff4
thisisnew <thisisnew@naver.com>
2025-06-23 06:04:33 +0900

    1543번 문서 검색

---
+++ src/baekjoon/string/No1543.java
@@ -0,0 +1,20 @@
+package baekjoon.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class No1543 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String sentence = br.readLine();
+        String word = br.readLine();
+
+        br.close();
+    }
+}
