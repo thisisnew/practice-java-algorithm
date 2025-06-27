3685c68c77545b22ad120f9e876cc7dc05577078
thisisnew <thisisnew@naver.com>
2025-06-28 08:34:01 +0900

    1543번 문서 검색

--- src/baekjoon/string/No1543.java
+++ src/baekjoon/string/No1543.java
@@ -3,9 +3,6 @@ package baekjoon.string;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class No1543 {
 
@@ -14,22 +11,16 @@ public class No1543 {
 
         String sentence = br.readLine();
         String word = br.readLine();
-        char firstChar = word.charAt(0);
-        int wordLen = word.length();
-
-        int lastIndex = sentence.length() - 1;
-        int idx = 0;
+        
         int count = 0;
-
-        while (idx < lastIndex) {
-            if (sentence.charAt(idx) != firstChar) {
-                idx++;
-                continue;
-            }
-
-            if (sentence.substring(idx, idx + wordLen).equals(word)) {
+        int idx = 0;
+        
+        while (idx <= sentence.length() - word.length()) {
+            if (sentence.startsWith(word, idx)) {
                 count++;
-                idx += wordLen;
+                idx += word.length();
+            } else {
+                idx++;
             }
         }
 
