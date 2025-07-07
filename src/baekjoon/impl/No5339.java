5a2758e27ff635b4cbf8760daa26122f08cd35be
thisisnew <thisisnew@naver.com>
2025-07-07 18:17:28 +0900

    5339번 콜센터

---
+++ src/baekjoon/impl/No5339.java
@@ -0,0 +1,19 @@
+package baekjoon.impl;
+
+import java.io.IOException;
+
+public class No5339 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/\\.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
