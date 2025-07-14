b5d8d4d41ed3efdd4e7fa1db3299e5bf27dfd480
thisisnew <thisisnew@naver.com>
2025-07-14 19:41:31 +0900

    10868번 최솟값

---
+++ src/baekjoon/datastructures/No10868.java
@@ -0,0 +1,17 @@
+package baekjoon.datastructures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class No10868 {
+
+    private static final String STR_ENTER = "ENTER";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.close();
+    }
+}
