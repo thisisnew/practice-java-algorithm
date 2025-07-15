0938e468cabd56d52f375760ed7ba0f2239fb5d2
thisisnew <thisisnew@naver.com>
2025-07-15 20:34:10 +0900

    10868번 최솟값

--- src/baekjoon/datastructures/No10868.java
+++ src/baekjoon/datastructures/No10868.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class No10868 {
 
@@ -12,6 +13,22 @@ public class No10868 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
         br.close();
     }
+
 }
