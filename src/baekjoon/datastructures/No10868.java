16cb2a65685d6ac5b985c9b28cfa1eb766d75268
thisisnew <thisisnew@naver.com>
2025-07-16 18:14:28 +0900

    10868번 최솟값

--- src/baekjoon/datastructures/No10868.java
+++ src/baekjoon/datastructures/No10868.java
@@ -3,13 +3,51 @@ package baekjoon.datastructures;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class No10868 {
 
-    private static final String STR_ENTER = "ENTER";
+    static class SegmentTree {
+        private int[] tree;
+        private int n;
+
+        public SegmentTree(int[] arr) {
+            n = arr.length;
+            tree = new int[4 * n];
+            build(arr, 1, 0, n - 1);
+        }
+
+        private void build(int[] arr, int node, int start, int end) {
+            if (start == end) {
+                tree[node] = arr[start];
+            } else {
+                int mid = (start + end) / 2;
+                build(arr, 2 * node, start, mid);
+                build(arr, 2 * node + 1, mid + 1, end);
+                tree[node] = Math.min(tree[2 * node], tree[2 * node + 1]);
+            }
+        }
+
+        public int query(int left, int right) {
+            return query(1, 0, n - 1, left, right);
+        }
+
+        private int query(int node, int start, int end, int left, int right) {
+            if (right < start || end < left) {
+                return Integer.MAX_VALUE;
+            }
+
+            if (left <= start && end <= right) {
+                return tree[node];
+            }
+
+            int mid = (start + end) / 2;
+            int leftMin = query(2 * node, start, mid, left, right);
+            int rightMin = query(2 * node + 1, mid + 1, end, left, right);
+
+            return Math.min(leftMin, rightMin);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,32 +56,23 @@ public class No10868 {
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        int[] numbers = new int[m + 1];
-
-        for (int i = 1; i <= m; i++) {
+        int[] numbers = new int[m];
+        for (int i = 0; i < m; i++) {
             numbers[i] = Integer.parseInt(br.readLine());
         }
 
+        SegmentTree segTree = new SegmentTree(numbers);
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
 
-            System.out.println(min(numbers, a, b));
+            sb.append(segTree.query(a, b)).append('\n');
         }
 
+        System.out.print(sb);
         br.close();
-    }
-
-    private static int min(int[] numbers, int start, int end) {
-        int min = Integer.MAX_VALUE;
-
-        for (int i = start; i <= end ; i++) {
-            if (numbers[i] < min) {
-                min = numbers[i];
-            }
-        }
-
-        return min;
     }
 }
