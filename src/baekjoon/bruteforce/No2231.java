e22cf6069d3463dde2e369c328fc84b792d3dc8b
thisisnew <thisisnew@naver.com>
2025-07-18 19:07:39 +0900

    2231번 분해합

---
+++ src/baekjoon/bruteforce/No2231.java
@@ -0,0 +1,15 @@
+package baekjoon.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No2231 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        br.close();
+    }
+}
