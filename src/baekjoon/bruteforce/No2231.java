c2e98bf7c075f186606897e0bfe14b5aecf01df4
thisisnew <thisisnew@naver.com>
2025-07-20 09:15:00 +0900

    2231번 분해합

--- src/baekjoon/bruteforce/No2231.java
+++ src/baekjoon/bruteforce/No2231.java
@@ -10,7 +10,9 @@ public class No2231 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = n; i >= 1; i--) {
+        for (int i = n-1; i >= 1; i--) {
+
+
 
         }
 
