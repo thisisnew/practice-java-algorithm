20c8c23e777a5b3d0600ec94171a49b5c04c6f05
thisisnew <thisisnew@naver.com>
2025-07-24 21:37:25 +0900

    1436번 영화감독 숌

--- src/baekjoon/bruteforce/No1436.java
+++ src/baekjoon/bruteforce/No1436.java
@@ -9,6 +9,10 @@ public class No1436 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        for (int i = 1; i <= n; i++) {
+
+        }
+
         br.close();
     }
 }
