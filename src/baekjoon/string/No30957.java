a993dbc3c05e51c5e157eda0add195929fcbb8d4
thisisnew <thisisnew@naver.com>
2025-08-06 20:59:57 +0900

    30957번 빅데이터 vs 정보보호 vs 인공지능

--- src/baekjoon/string/No30957.java
+++ src/baekjoon/string/No30957.java
@@ -13,17 +13,21 @@ public class No30957 {
         int n = Integer.parseInt(br.readLine());
         String input = br.readLine();
 
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('B', 0);
-        map.put('S', 0);
-        map.put('A', 0);
+        int bCnt = 0;
+        int sCnt = 0;
+        int aCnt = 0;
 
         for (int i = 0; i < n; i++) {
             char c = input.charAt(i);
-            map.put(c, map.get(c) + 1);
+
+            switch (c) {
+                case 'A': aCnt++; break;
+                case 'B': bCnt++; break;
+                case 'S': sCnt++; break;
+                default: break;
+            }
         }
 
-        System.out.println(map);
 
         br.close();
     }
