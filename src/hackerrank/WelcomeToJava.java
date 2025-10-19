c68d0092d0d05bd978e138b417233022b40fbeaf
thisisnew <thisisnew@naver.com>
2025-10-19 23:14:19 +0900

    해커랭크 Welcome to Java!

--- src/hackerrank/WelcomeToJava.java
+++ src/hackerrank/WelcomeToJava.java
@@ -3,6 +3,7 @@ package hackerrank;
 public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Hello, World.");
+
         System.out.println("Hello, Java.");
     }
 }
