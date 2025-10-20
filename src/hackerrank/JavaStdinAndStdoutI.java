0c05d9be3b1418c3b8125686012fc2f2a8d5553f
thisisnew <thisisnew@naver.com>
2025-10-20 22:15:36 +0900

    해커랭크 Java Stdin and Stdout I

---
+++ src/hackerrank/JavaStdinAndStdoutI.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaStdinAndStdoutI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
