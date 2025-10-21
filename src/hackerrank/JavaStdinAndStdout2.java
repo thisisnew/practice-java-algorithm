34d9c1693a805bace7ab50b3c9e840371d7e8682
thisisnew <thisisnew@naver.com>
2025-10-21 19:57:35 +0900

    해커랭크 Java Stdin and Stdout II

--- src/hackerrank/JavaStdinAndStdout2.java
+++ src/hackerrank/JavaStdinAndStdout2.java
@@ -8,12 +8,12 @@ public class JavaStdinAndStdout2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        var a = br.readLine();
+        var a = Integer.parseInt(br.readLine());
         var b = Double.parseDouble(br.readLine());
-        var c = Integer.parseInt(br.readLine());
+        var c = br.readLine();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println("String: " + c);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
     }
 }
