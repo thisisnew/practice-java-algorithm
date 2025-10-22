56db126fd03bd2011a30259f648a3e701fc048a1
thisisnew <thisisnew@naver.com>
2025-10-22 21:58:29 +0900

    해커랭크 Java Output Formatting

--- src/hackerrank/JavaOutputFormatting.java
+++ src/hackerrank/JavaOutputFormatting.java
@@ -8,14 +8,35 @@ public class JavaOutputFormatting {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        var a = br.readLine();
-        var b = br.readLine();
-        var c = br.readLine();
+        var a = br.readLine().split(" ");
+        var b = br.readLine().split(" ");
+        var c = br.readLine().split(" ");
 
         System.out.println("================================");
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        System.out.println(addWhiteSpace(a[0]) + leadingZero(a[1]));
+        System.out.println(addWhiteSpace(b[0]) + leadingZero(b[1]));
+        System.out.println(addWhiteSpace(c[0]) + leadingZero(c[1]));
         System.out.println("================================");
+    }
+
+    private static String addWhiteSpace(String s) {
+        StringBuilder result = new StringBuilder(s);
+
+        while (result.length() < 15) {
+            result.append(" ");
+        }
+
+        return result.toString();
+    }
+
+    private static String leadingZero(String s) {
+
+        StringBuilder result = new StringBuilder(s);
+
+        while (result.length() < 3) {
+            result.insert(0, "0");
+        }
+
+        return result.toString();
     }
 }
