54f96ef0fd727690ecd24baa55f14266171fa1b8
thisisnew <thisisnew@naver.com>
2025-10-22 22:03:33 +0900

    해커랭크 Java Loops I

---
+++ src/hackerrank/JavaLoopsI.java
@@ -0,0 +1,16 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoopsI {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(String.format("%d x %d = %d", n, i, n * i));
+        }
+    }
+}
