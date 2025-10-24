7d29b4e9d092b9310646c614f13cd9e599e9e455
thisisnew <thisisnew@naver.com>
2025-10-24 23:15:35 +0900

    해커랭크 Java Datatypes

---
+++ src/hackerrank/JavaDataTypes.java
@@ -0,0 +1,30 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaDataTypes {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            boolean end = false;
+
+            while (!end){
+                try{
+                    int n = Integer.parseInt(br.readLine());
+
+
+
+                } catch (Exception e){
+                    end = true;
+                }
+            }
+
+
+        }
+
+    }
+}
