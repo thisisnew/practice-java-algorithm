16d9aa75707993eec871548e3a328dca6b452a87
thisisnew <thisisnew@naver.com>
2025-10-27 20:11:34 +0900

    해커랭크 Java Static Initializer Block

---
+++ src/hackerrank/JavaStaticInitializerBlock.java
@@ -0,0 +1,30 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+
+    private static int B;
+    private static int H;
+    private static boolean flag = true;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+
+        if((B <= 0 || H <= 0)) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+        if(flag){
+            int area = B * H;
+            System.out.print(area);
+        }
+    }
+}
