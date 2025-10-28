8274bb494fef9f42e7d8893cd8ce97d41daccad1
thisisnew <thisisnew@naver.com>
2025-10-28 19:07:54 +0900

    해커랭크 Java Date and Time

--- src/hackerrank/JavaDateAndTime.java
+++ src/hackerrank/JavaDateAndTime.java
@@ -1,4 +1,21 @@
 package hackerrank;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.util.StringTokenizer;
+
 public class JavaDateAndTime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+        int year = Integer.parseInt(st.nextToken());
+
+        LocalDateTime dateTime = LocalDateTime.of(year, month, day, 0, 0);
+        System.out.println(dateTime.getDayOfWeek());
+    }
 }
