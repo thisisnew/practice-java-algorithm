574d65542d5afafd40bd35de38cc834fffb31168
thisisnew <thisisnew@naver.com>
2025-10-29 21:57:40 +0900

    해커랭크 Java Currency Formatter

--- src/hackerrank/JavaCurrencyFormatter.java
+++ src/hackerrank/JavaCurrencyFormatter.java
@@ -9,38 +9,19 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class JavaCurrencyFormatter {
-    private static String normalizeSpaces(String s) {
-        return s.replace('\u00A0', ' ').replace('\u202F', ' ');
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         double payment = Double.parseDouble(br.readLine());
 
         NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
-
         NumberFormat india = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
-        if (india instanceof DecimalFormat) {
-            DecimalFormat df = (DecimalFormat) india;
-            DecimalFormatSymbols syms = df.getDecimalFormatSymbols();
-            syms.setCurrencySymbol("Rs.");
-            df.setDecimalFormatSymbols(syms);
-        }
-
         NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        if (china instanceof DecimalFormat) {
-            DecimalFormat df = (DecimalFormat) china;
-            DecimalFormatSymbols syms = df.getDecimalFormatSymbols();
-            syms.setCurrencySymbol("￥"); // U+FFE5
-            df.setDecimalFormatSymbols(syms);
-        }
-
         NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
 
-        String usOut = normalizeSpaces(us.format(payment));
-        String inOut = normalizeSpaces(india.format(payment));
-        String cnOut = normalizeSpaces(china.format(payment));
-        String frOut = normalizeSpaces(france.format(payment));
+        String usOut = us.format(payment);
+        String inOut = india.format(payment);
+        String cnOut = china.format(payment);
+        String frOut = france.format(payment);
 
         System.out.println("US: " + usOut);
         System.out.println("India: " + inOut);
