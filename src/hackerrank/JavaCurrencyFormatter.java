754801656f77a15a6f408ba2094123e038bc64f8
thisisnew <thisisnew@naver.com>
2025-10-29 19:32:12 +0900

    해커랭크 Java Currency Formatter

--- src/hackerrank/JavaCurrencyFormatter.java
+++ src/hackerrank/JavaCurrencyFormatter.java
@@ -9,24 +9,42 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class JavaCurrencyFormatter {
+    private static String normalizeSpaces(String s) {
+        return s.replace('\u00A0', ' ').replace('\u202F', ' ');
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        var payment = Double.parseDouble(br.readLine());
-        var usCurrency = NumberFormat.getCurrencyInstance(Locale.US);
-        var indiaCurrency = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
-        if (indiaCurrency instanceof DecimalFormat) {
-            DecimalFormat df = (DecimalFormat) indiaCurrency;
-            DecimalFormatSymbols symbols = df.getDecimalFormatSymbols();
-            symbols.setCurrencySymbol("Rs.");
-            df.setDecimalFormatSymbols(symbols);
+        double payment = Double.parseDouble(br.readLine());
+
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+
+        NumberFormat india = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        if (india instanceof DecimalFormat) {
+            DecimalFormat df = (DecimalFormat) india;
+            DecimalFormatSymbols syms = df.getDecimalFormatSymbols();
+            syms.setCurrencySymbol("Rs.");
+            df.setDecimalFormatSymbols(syms);
         }
 
-        var chinaCurrency = NumberFormat.getCurrencyInstance(Locale.CHINA);
-        var frenchCurrency = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        if (china instanceof DecimalFormat) {
+            DecimalFormat df = (DecimalFormat) china;
+            DecimalFormatSymbols syms = df.getDecimalFormatSymbols();
+            syms.setCurrencySymbol("￥"); // U+FFE5
+            df.setDecimalFormatSymbols(syms);
+        }
 
-        System.out.println("US: " + usCurrency.format(payment));
-        System.out.println("India: " + indiaCurrency.format(payment));
-        System.out.println("China: " + chinaCurrency.format(payment));
-        System.out.println("France: " + frenchCurrency.format(payment));
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        String usOut = normalizeSpaces(us.format(payment));
+        String inOut = normalizeSpaces(india.format(payment));
+        String cnOut = normalizeSpaces(china.format(payment));
+        String frOut = normalizeSpaces(france.format(payment));
+
+        System.out.println("US: " + usOut);
+        System.out.println("India: " + inOut);
+        System.out.println("China: " + cnOut);
+        System.out.println("France: " + frOut);
     }
 }
