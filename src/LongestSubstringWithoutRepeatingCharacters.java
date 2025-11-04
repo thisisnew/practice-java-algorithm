bd9c75158b5ce23f949898fc34dd3f6444cbeebe
thisisnew <thisisnew@naver.com>
2025-11-04 22:31:49 +0900

    leetcode Longest Substring Without Repeating Characters

--- src/LongestSubstringWithoutRepeatingCharacters.java
+++ src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,9 +1,28 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class LongestSubstringWithoutRepeatingCharacters {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(lengthOfLongestSubstring(br.readLine()));
+    }
 
+    public static int lengthOfLongestSubstring(String s) {
+        var set = new HashSet<Character>();
+        var result = 0;
+
+        for (var i = 0; i < s.length(); i++) {
+            var c = s.charAt(i);
+
+            if (!set.contains(c)) {
+                result++;
+            }
+
+            set.add(c);
+        }
+
+        return result;
     }
 }
