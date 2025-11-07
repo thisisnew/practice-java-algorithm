14edc9cb48ddfa5d7bbefca3e499ef7052a7cbe9
thisisnew <thisisnew@naver.com>
2025-11-08 00:37:18 +0900

    leetcode Zigzag Conversion

---
+++ src/leetcode/ZigzagConversion.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ZigzagConversion {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        var s = br.readLine();
+        var rows = Integer.parseInt(br.readLine());
+
+        System.out.println(convert(s, rows));
+    }
+
+    public static String convert(String s, int numRows) {
+        return "";
+    }
+}
