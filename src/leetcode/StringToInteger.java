a38a36fcf3d835c44e5effb6d03e397121dc9306
thisisnew <thisisnew@naver.com>
2025-11-13 22:42:18 +0900

    leetcode String to Integer (atoi)

--- src/leetcode/StringToInteger.java
+++ src/leetcode/StringToInteger.java
@@ -3,6 +3,7 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class StringToInteger {
     public static void main(String[] args) throws IOException {
@@ -11,6 +12,43 @@ public class StringToInteger {
     }
 
     private static int myAtoi(String s) {
-        return "";
+        if (Objects.isNull(s) || s.isBlank()) {
+            return 0;
+        }
+
+        var value = s.trim();
+        var multiplier = value.charAt(0) == '-' ? -1 : 1;
+
+        var sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c < '0' || c > '9') {
+                if (i > 0) {
+                    break;
+                }
+            }
+
+            sb.append(c);
+        }
+
+        try {
+            return multiplier * Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            if (multiplier == 1) {
+                return 2147483647;
+            }
+
+            return -2147483648;
+        }
+    }
+
+    private static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
