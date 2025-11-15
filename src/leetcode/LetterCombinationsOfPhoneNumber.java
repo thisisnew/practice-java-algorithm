69a19ec600d5956b1bfb73bbd4a3358ba2859df8
thisisnew <thisisnew@naver.com>
2025-11-15 18:12:04 +0900

    leetcode Letter Combinations of a Phone Number

---
+++ src/leetcode/LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class LetterCombinationsOfPhoneNumber {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(letterCombinations(br.readLine()));
+    }
+
+    private static List<String> letterCombinations(String digits) {
+        return List.of();
+    }
+}
