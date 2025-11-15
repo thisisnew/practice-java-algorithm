4f253a6a0e2599fcf21ab9e2e3c9a5bfc66a78e0
thisisnew <thisisnew@naver.com>
2025-11-15 18:20:52 +0900

    leetcode Letter Combinations of a Phone Number

--- src/leetcode/LetterCombinationsOfPhoneNumber.java
+++ src/leetcode/LetterCombinationsOfPhoneNumber.java
@@ -3,15 +3,38 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class LetterCombinationsOfPhoneNumber {
+
+    private static final Map<Character, List<Character>> CHARACTER_LIST_MAP = Map.of(
+            '2', List.of('a', 'b', 'c'),
+            '3', List.of('d', 'e', 'f'),
+            '4', List.of('g', 'h', 'i'),
+            '5', List.of('j', 'k', 'l'),
+            '6', List.of('m', 'n', 'o'),
+            '7', List.of('p', 'q', 'r', 's'),
+            '8', List.of('t', 'u', 'v'),
+            '9', List.of('w', 'x', 'y', 'z')
+    );
+
     public static void main(String[] args) throws IOException {
         var br =  new BufferedReader(new InputStreamReader(System.in));
         System.out.println(letterCombinations(br.readLine()));
     }
 
     private static List<String> letterCombinations(String digits) {
+
+        var list = new ArrayList<>();
+
+        for (var c : digits.toCharArray()) {
+
+
+
+        }
+
         return List.of();
     }
 }
