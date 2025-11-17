b9dab11f1e39ef1d3c441b61a47f06e2d7573c92
thisisnew <thisisnew@naver.com>
2025-11-17 19:09:48 +0900

    leetcode Letter Combinations of a Phone Number

--- src/leetcode/LetterCombinationsOfPhoneNumber.java
+++ src/leetcode/LetterCombinationsOfPhoneNumber.java
@@ -9,15 +9,15 @@ import java.util.Map;
 
 public class LetterCombinationsOfPhoneNumber {
 
-    private static final Map<Character, List<Character>> CHARACTER_LIST_MAP = Map.of(
-            '2', List.of('a', 'b', 'c'),
-            '3', List.of('d', 'e', 'f'),
-            '4', List.of('g', 'h', 'i'),
-            '5', List.of('j', 'k', 'l'),
-            '6', List.of('m', 'n', 'o'),
-            '7', List.of('p', 'q', 'r', 's'),
-            '8', List.of('t', 'u', 'v'),
-            '9', List.of('w', 'x', 'y', 'z')
+    private static final Map<String, List<String>> CHARACTER_LIST_MAP = Map.of(
+            "2", List.of("a", "b", "c"),
+            "3", List.of("d", "e", "f"),
+            "4", List.of("g", "h", "i"),
+            "5", List.of("j", "k", "l"),
+            "6", List.of("m", "n", "o"),
+            "7", List.of("p", "q", "r", "s"),
+            "8", List.of("t", "u", "v"),
+            "9", List.of("w", "x", "y", "z")
     );
 
     public static void main(String[] args) throws IOException {
@@ -27,35 +27,38 @@ public class LetterCombinationsOfPhoneNumber {
 
     private static List<String> letterCombinations(String digits) {
 
-        List<List<Character>> list = new ArrayList<>();
+        List<List<String>> charList = new ArrayList<>();
 
         for (var c : digits.toCharArray()) {
-            list.add(CHARACTER_LIST_MAP.get(c));
+            charList.add(CHARACTER_LIST_MAP.get(c));
         }
 
-        if (list.isEmpty()) {
+        if (charList.isEmpty()) {
             return new ArrayList<>();
         }
 
         var result = new ArrayList<String>();
 
-
-        for (var i = 0; i < list.get(0).size(); i++) {
-            var first = list.get(0).get(i);
-            var l = new ArrayList<Character>();
+        for (var i = 0; i < charList.get(0).size(); i++) {
+            var list = new ArrayList<String>();
+            list.add(charList.get(0).get(i));
 
             var idx = 1;
-            while(idx < list.size()) {
-                var chars = list.get(idx);
+            while(idx < charList.size()) {
+                var chars = charList.get(idx);
 
                 for (var c : chars) {
-
+                    for (var s : list) {
+                        list.add(s + c);
+                        list.remove(s);
+                    }
                 }
 
                 idx++;
+                result.addAll(list);
             }
 
-
+            System.out.println(result);
         }
 
         return result;
