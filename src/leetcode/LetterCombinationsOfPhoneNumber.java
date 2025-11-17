cab6b50e625b6e7e72f989255a0f4a9c3baf47de
thisisnew <thisisnew@naver.com>
2025-11-17 19:11:19 +0900

    leetcode Letter Combinations of a Phone Number

--- src/leetcode/LetterCombinationsOfPhoneNumber.java
+++ src/leetcode/LetterCombinationsOfPhoneNumber.java
@@ -9,15 +9,15 @@ import java.util.Map;
 
 public class LetterCombinationsOfPhoneNumber {
 
-    private static final Map<String, List<String>> CHARACTER_LIST_MAP = Map.of(
-            "2", List.of("a", "b", "c"),
-            "3", List.of("d", "e", "f"),
-            "4", List.of("g", "h", "i"),
-            "5", List.of("j", "k", "l"),
-            "6", List.of("m", "n", "o"),
-            "7", List.of("p", "q", "r", "s"),
-            "8", List.of("t", "u", "v"),
-            "9", List.of("w", "x", "y", "z")
+    private static final Map<Character, List<String>> CHARACTER_LIST_MAP = Map.of(
+            '2', List.of("a", "b", "c"),
+            '3', List.of("d", "e", "f"),
+            '4', List.of("g", "h", "i"),
+            '5', List.of("j", "k", "l"),
+            '6', List.of("m", "n", "o"),
+            '7', List.of("p", "q", "r", "s"),
+            '8', List.of("t", "u", "v"),
+            '9', List.of("w", "x", "y", "z")
     );
 
     public static void main(String[] args) throws IOException {
