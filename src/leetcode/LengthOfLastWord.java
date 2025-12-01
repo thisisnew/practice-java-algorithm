0eca51726299b51e2cab22c92843e9e624f634f4
thisisnew <thisisnew@naver.com>
2025-12-01 21:55:28 +0900

    leetcode Length of Last Word

---
+++ src/leetcode/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(lengthOfLastWord(br.readLine()));
+    }
+
+    private static int lengthOfLastWord(String s) {
+        return 0;
+    }
+}
