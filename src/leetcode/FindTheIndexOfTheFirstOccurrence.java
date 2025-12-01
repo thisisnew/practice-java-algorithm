365c0d697bb2c8a2729d7e90701dbcc386305d15
thisisnew <thisisnew@naver.com>
2025-12-01 20:52:09 +0900

    leetcode Find the Index of the First Occurrence in a String

--- src/leetcode/FindTheIndexOfTheFirstOccurrence.java
+++ src/leetcode/FindTheIndexOfTheFirstOccurrence.java
@@ -7,7 +7,18 @@ import java.io.InputStreamReader;
 public class FindTheIndexOfTheFirstOccurrence {
     public static void main(String[] args) throws IOException {
         var br =  new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        String line = br.readLine();
+
+        line = line.replace("haystack =", "")
+                .replace("needle =", "")
+                .replace("\"", "")
+                .replace(",", "")
+                .trim();
+
+        String[] arr = line.split("\\s+"); // 공백 기준 split
+        String haystack = arr[0];
+        String needle = arr[1];
+
         System.out.println(strStr("" ,""));
     }
 
