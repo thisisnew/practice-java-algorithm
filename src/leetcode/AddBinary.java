abdec58d0ef1225576d78afb3a0202ca7b8c539c
thisisnew <thisisnew@naver.com>
2025-12-02 20:06:06 +0900

    leetcode Add Binary

---
+++ src/leetcode/AddBinary.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AddBinary {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(addBinary("", ""));
+    }
+
+    private static String addBinary(String a, String b) {
+        return "";
+    }
+}
