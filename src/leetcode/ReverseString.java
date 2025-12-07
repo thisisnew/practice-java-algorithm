e1fc4e09729768dbe912920e81f88846228c5dc1
thisisnew <thisisnew@naver.com>
2025-12-08 06:17:30 +0900

    leetcode Reverse String

--- src/leetcode/ReverseString.java
+++ src/leetcode/ReverseString.java
@@ -3,6 +3,7 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ReverseString {
 
@@ -15,7 +16,14 @@ public class ReverseString {
 
     private static void reverseString(char[] s) {
 
+        char[] result = new char[s.length];
 
+        var index = 0;
+        for (int i = s.length-1; i >= 0 ; i--) {
+            result[index] = s[i];
+            index++;
+        }
 
+        System.out.println(Arrays.toString(result));
     }
 }
