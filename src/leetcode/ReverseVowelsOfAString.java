e8d248045dcaaab9e70b70bbfd1368ba53636c65
thisisnew <thisisnew@naver.com>
2025-12-10 20:42:22 +0900

    leetcode Reverse Vowels of a String

--- src/leetcode/ReverseVowelsOfAString.java
+++ src/leetcode/ReverseVowelsOfAString.java
@@ -22,7 +22,7 @@ public class ReverseVowelsOfAString {
         for (int i = 0; i < s.length(); i++) {
             var c = s.charAt(i);
 
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            if (isVowel(c)) {
                 map.put(i, c);
                 indexes.push(i);
             }
