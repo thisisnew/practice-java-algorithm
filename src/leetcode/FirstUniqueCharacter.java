3c39bf5c41f27261e69a863281db09cd3d563f1c
thisisnew <thisisnew@naver.com>
2025-12-14 09:34:25 +0900

    leetcode First Unique Character in a String

---
+++ src/leetcode/FirstUniqueCharacter.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FirstUniqueCharacter {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(firstUniqChar(br.readLine()));
+    }
+
+    private static int firstUniqChar(String s) {
+        return 0;
+    }
+}
