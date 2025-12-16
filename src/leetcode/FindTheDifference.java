e39603679bcd40279b71c678f49ce29729bc281a
thisisnew <thisisnew@naver.com>
2025-12-16 21:06:56 +0900

    leetcode Find the Difference

---
+++ src/leetcode/FindTheDifference.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FindTheDifference {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        var a = br.readLine();
+        var b = br.readLine();
+        System.out.println(findTheDifference(a, b));
+    }
+
+    private static char findTheDifference(String s, String t) {
+        return 'a';
+    }
+}
