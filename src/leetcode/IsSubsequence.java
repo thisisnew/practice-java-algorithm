c3786453d92e39c6df083da42c1a7dcbb726ff97
thisisnew <thisisnew@naver.com>
2025-12-21 01:34:54 +0900

    leetcode Is Subsequence

---
+++ src/leetcode/IsSubsequence.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class IsSubsequence {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        var s = br.readLine();
+        var t = br.readLine();
+        System.out.println(isSubsequence(s, t));
+    }
+
+    private static boolean isSubsequence(String s, String t) {
+        return false;
+    }
+}
