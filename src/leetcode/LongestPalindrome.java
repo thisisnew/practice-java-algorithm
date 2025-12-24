03e0844fe58c92f4d29424b819efa4a42d8d7ba0
thisisnew <thisisnew@naver.com>
2025-12-25 02:09:35 +0900

    leetcode Longest Palindrome

---
+++ src/leetcode/LongestPalindrome.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LongestPalindrome {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(longestPalindrome(br.readLine()));
+    }
+
+    private static int longestPalindrome(String s) {
+        return 0;
+    }
+}
