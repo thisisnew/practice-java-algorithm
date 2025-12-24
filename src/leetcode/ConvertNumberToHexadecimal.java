e2408043653cec3ff688f27fd4509b2b93437ef5
thisisnew <thisisnew@naver.com>
2025-12-24 22:23:22 +0900

    leetcode Convert a Number to Hexadecimal

---
+++ src/leetcode/ConvertNumberToHexadecimal.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConvertNumberToHexadecimal {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(toHex(Integer.parseInt(br.readLine())));
+    }
+
+    private static String toHex(int num) {
+        return "";
+    }
+}
