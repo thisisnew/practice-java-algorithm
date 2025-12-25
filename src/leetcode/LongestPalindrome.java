c9318cef4b4df2e12159ed54e5a2007060785903
thisisnew <thisisnew@naver.com>
2025-12-25 09:51:37 +0900

    leetcode Longest Palindrome

--- src/leetcode/LongestPalindrome.java
+++ src/leetcode/LongestPalindrome.java
@@ -3,6 +3,7 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class LongestPalindrome {
     public static void main(String[] args) throws IOException {
@@ -11,6 +12,27 @@ public class LongestPalindrome {
     }
 
     private static int longestPalindrome(String s) {
-        return 0;
+
+        var numMap = new HashMap<Character, Integer>();
+        var maxOddNum = 0;
+
+        for (var c : s.toCharArray()) {
+            numMap.put(c, numMap.getOrDefault(c, 0) + 1);
+        }
+
+        var result = 0;
+
+        for (var cnt : numMap.values()) {
+            if (cnt == 0) {
+                result += cnt;
+                continue;
+            }
+
+            if (maxOddNum <= cnt) {
+                maxOddNum = cnt;
+            }
+        }
+
+        return result + maxOddNum;
     }
 }
