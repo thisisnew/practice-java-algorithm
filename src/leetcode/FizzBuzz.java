d2322c842d18f0086a31f97c6c58875496389d80
thisisnew <thisisnew@naver.com>
2025-12-28 17:33:29 +0900

    leetcode Fizz Buzz

---
+++ src/leetcode/FizzBuzz.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(fizzBuzz(Integer.parseInt(br.readLine())));
+    }
+
+    private static List<String> fizzBuzz(int n) {
+        return List.of();
+    }
+}
