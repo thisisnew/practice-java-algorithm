144766dbdc14eaa3ffb73f5b7af5344e01cbc4eb
thisisnew <thisisnew@naver.com>
2025-12-28 21:45:34 +0900

    leetcode Add Strings

---
+++ src/leetcode/AddStrings.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AddStrings {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        var num1 = br.readLine();
+        var num2 = br.readLine();
+        System.out.println(addStrings(num1, num2));
+    }
+
+    private static String addStrings(String num1, String num2) {
+        return "";
+    }
+}
