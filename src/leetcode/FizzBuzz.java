bab158015ab7958a0ce9afcb3c1a99cde8e18737
thisisnew <thisisnew@naver.com>
2025-12-28 21:43:02 +0900

    leetcode Fizz Buzz

--- src/leetcode/FizzBuzz.java
+++ src/leetcode/FizzBuzz.java
@@ -15,10 +15,27 @@ public class FizzBuzz {
     private static List<String> fizzBuzz(int n) {
         var result = new ArrayList<String>();
 
-        for (int i = 0; i < n; i++) {
-
+        for (int i = 1; i <= n; i++) {
+            result.add(word(i));
         }
 
         return result;
+    }
+
+    private static String word(int n) {
+
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (n % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(n);
     }
 }
