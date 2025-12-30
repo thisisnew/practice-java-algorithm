79c4c9ceac79648ee9fd4b7081c8f1d30293613c
thisisnew <thisisnew@naver.com>
2025-12-30 20:54:42 +0900

    leetcode Add Strings

--- src/leetcode/AddStrings.java
+++ src/leetcode/AddStrings.java
@@ -13,7 +13,27 @@ public class AddStrings {
     }
 
     private static String addStrings(String num1, String num2) {
-        var sum = Double.parseDouble(num1) + Double.parseDouble(num2);
-        return String.valueOf((long) sum);
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+
+            carry = sum / 10;
+            sb.append(sum % 10);
+        }
+
+        return sb.reverse().toString();
     }
 }
