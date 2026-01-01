9aa666ce72a40dc252212c6bd22423a278551a06
thisisnew <thisisnew@naver.com>
2026-01-01 19:55:34 +0900

    leetcode Number of Segments in a String

--- src/leetcode/NumberOfSegmentsInString.java
+++ src/leetcode/NumberOfSegmentsInString.java
@@ -3,6 +3,7 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class NumberOfSegmentsInString {
     public static void main(String[] args) throws IOException {
@@ -11,6 +12,20 @@ public class NumberOfSegmentsInString {
     }
 
     private static int countSegments(String s) {
-        return s.split(" ").length;
+        var list = new ArrayList<String>();
+
+        var sb = new StringBuilder();
+        for (var i=0; i<s.length();i++) {
+            var c = s.charAt(i);
+
+            if (i < s.length()-1 && c != ' ') {
+                sb.append(c);
+                continue;
+            }
+
+            list.add(sb.toString());
+        }
+
+        return list.size();
     }
 }
