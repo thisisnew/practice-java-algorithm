2e514c26c79063c3641136bd854b71e60db870d6
thisisnew <thisisnew@naver.com>
2026-01-01 22:08:55 +0900

    leetcode Repeated Substring Pattern

---
+++ src/leetcode/RepeatedSubstringPattern.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RepeatedSubstringPattern {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(repeatedSubstringPattern(br.readLine()));
+    }
+
+    private static boolean repeatedSubstringPattern(String s) {
+        return true;
+    }
+}
