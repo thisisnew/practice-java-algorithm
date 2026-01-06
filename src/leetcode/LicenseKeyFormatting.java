494507cdfd6b7b075159ac1fb05bf92c16c06539
thisisnew <thisisnew@naver.com>
2026-01-06 19:19:12 +0900

    leetcode License Key Formatting

--- src/leetcode/LicenseKeyFormatting.java
+++ src/leetcode/LicenseKeyFormatting.java
@@ -3,6 +3,7 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class LicenseKeyFormatting {
     public static void main(String[] args) throws IOException {
@@ -13,6 +14,27 @@ public class LicenseKeyFormatting {
     }
 
     private static String licenseKeyFormatting(String s, int k) {
+
+        var replaced = s.replaceAll("-", "");
+        var len = replaced.length();
+        var list = new ArrayList<String[]>();
+
+        while(len > 0) {
+            if (len < k) {
+                list.add(new String[len]);
+            } else {
+                list.add(new String[k]);
+            }
+
+            len -= k;
+        }
+
+        for (var i = list.size() - 1; i >= 0; i--) {
+
+
+
+        }
+
         return "";
     }
 }
