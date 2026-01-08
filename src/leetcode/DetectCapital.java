e64d1b178d37f41957270d0a7a0d4d917d93d206
thisisnew <thisisnew@naver.com>
2026-01-08 20:09:13 +0900

    leetcode Detect Capital

---
+++ src/leetcode/DetectCapital.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DetectCapital {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(detectCapitalUse(br.readLine()));
+    }
+
+    private static boolean detectCapitalUse(String word) {
+        return true;
+    }
+}
