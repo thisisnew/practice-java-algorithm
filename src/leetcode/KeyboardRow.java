deb9b367ce90e3992f9ca4302733829744387611
thisisnew <thisisnew@naver.com>
2026-01-08 19:35:42 +0900

    leetcode Keyboard Row

--- src/leetcode/KeyboardRow.java
+++ src/leetcode/KeyboardRow.java
@@ -6,6 +6,11 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class KeyboardRow {
+
+    private static final String[] FIST_ROW = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p"};
+    private static final String[] SECOND_ROW = {"a", "s", "d", "f", "g", "h", "j", "k", "l"};
+    private static final String[] THIRD_ROW = {"z", "x", "c", "v", "b", "n", "m"};
+
     public static void main(String[] args) throws IOException {
         var br =  new BufferedReader(new InputStreamReader(System.in));
         System.out.println(Arrays.toString(findWords(new String[]{br.readLine()})));
