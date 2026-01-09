e24b85d7445b7fefb7d2a637489168afdd6820e0
thisisnew <thisisnew@naver.com>
2026-01-09 15:17:55 +0900

    leetcode Detect Capital

--- src/leetcode/DetectCapital.java
+++ src/leetcode/DetectCapital.java
@@ -3,14 +3,27 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Set;
 
 public class DetectCapital {
+
+    private static final Set<Character> UPPER_CHARS = Set.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',  'M', 'N', 'O', 'P',  'Q', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+
     public static void main(String[] args) throws IOException {
         var br =  new BufferedReader(new InputStreamReader(System.in));
         System.out.println(detectCapitalUse(br.readLine()));
     }
 
     private static boolean detectCapitalUse(String word) {
+
+        for(char c : word.toCharArray()) {
+            if(UPPER_CHARS.contains(c)) {
+                continue;
+            }
+
+            return false;
+        }
+
         return true;
     }
 }
