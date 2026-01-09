4ab14f195257f765fa5d6671d6064581daf36306
thisisnew <thisisnew@naver.com>
2026-01-09 22:31:40 +0900

    leetcode Detect Capital

--- src/leetcode/DetectCapital.java
+++ src/leetcode/DetectCapital.java
@@ -3,11 +3,13 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 import java.util.Set;
 
 public class DetectCapital {
 
-    private static final Set<Character> UPPER_CHARS = Set.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',  'M', 'N', 'O', 'P',  'Q', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+    private static final Set<Character> UPPER_CHARS = Set.of('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+    private static final Set<Character> LOWER_CHARS = Set.of('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
 
     public static void main(String[] args) throws IOException {
         var br =  new BufferedReader(new InputStreamReader(System.in));
@@ -15,8 +17,14 @@ public class DetectCapital {
     }
 
     private static boolean detectCapitalUse(String word) {
+        if (Objects.isNull(word) || word.isBlank()) return false;
 
-        for(char c : word.toCharArray()) {
+        var upperCnt = 0;
+        var lowerCnt = 0;
+        var chars = word.toCharArray();
+        var firstChar = chars[0];
+
+        for(char c : chars) {
             if(UPPER_CHARS.contains(c)) {
                 continue;
             }
