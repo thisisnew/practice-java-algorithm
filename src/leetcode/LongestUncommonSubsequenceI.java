bed8c6f77b1bf3fdc6f08f8dabf48dbab72511c3
thisisnew <thisisnew@naver.com>
2026-01-10 15:34:30 +0900

    leetcode Longest Uncommon Subsequence I

---
+++ src/leetcode/LongestUncommonSubsequenceI.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LongestUncommonSubsequenceI {
+
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(findLUSlength(br.readLine(), br.readLine()));
+    }
+
+    private static int findLUSlength(String a, String b) {
+        return 0;
+    }
+}
