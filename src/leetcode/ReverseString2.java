1b5ceff75c01746502c2545f3b8e9ce01214c000
thisisnew <thisisnew@naver.com>
2026-01-12 19:29:42 +0900

    leetcode Reverse String 2

---
+++ src/leetcode/ReverseString2.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseString2 {
+
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(reverseStr(br.readLine(), Integer.parseInt(br.readLine())));
+    }
+
+    private static String reverseStr(String s, int k) {
+        return "";
+    }
+}
