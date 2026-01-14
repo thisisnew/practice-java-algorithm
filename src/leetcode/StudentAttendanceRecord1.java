c64c1437341d3a99ad306a0afc884172afa6aeeb
thisisnew <thisisnew@naver.com>
2026-01-14 21:45:43 +0900

    leetcode Student Attendance Record I

---
+++ src/leetcode/StudentAttendanceRecord1.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StudentAttendanceRecord1 {
+
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(checkRecord(br.readLine()));
+    }
+
+    private static boolean checkRecord(String s) {
+        return false;
+    }
+}
