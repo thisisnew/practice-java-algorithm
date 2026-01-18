ff6e9c3459cc1b8d3198b39be82c133469366e52
thisisnew <thisisnew@naver.com>
2026-01-18 09:12:10 +0900

    leetcode Valid Palindrome II

---
+++ src/leetcode/ValidPalindrome2.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ValidPalindrome2 {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(validPalindrome(br.readLine()));
+    }
+
+    private static boolean validPalindrome(String moves) {
+
+        for (var move : moves.toCharArray()) {
+
+        }
+
+        return false;
+    }
+}
