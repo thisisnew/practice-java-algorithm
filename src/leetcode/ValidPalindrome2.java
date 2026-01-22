30eb5bdd0dbea1c1b06d72c30139c89cb65c75d8
thisisnew <thisisnew@naver.com>
2026-01-22 19:21:05 +0900

    leetcode Valid Palindrome II

--- src/leetcode/ValidPalindrome2.java
+++ src/leetcode/ValidPalindrome2.java
@@ -3,7 +3,6 @@ package leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class ValidPalindrome2 {
     public static void main(String[] args) throws IOException {
@@ -12,12 +11,26 @@ public class ValidPalindrome2 {
     }
 
     private static boolean validPalindrome(String s) {
-        var stack = new Stack<Character>();
+        int l = 0, r = s.length() - 1;
 
-        for (var c : s.toCharArray()) {
-            stack.push(c);
+        while (l < r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } else {
+                // 삭제 1회 사용: 왼쪽 제거 또는 오른쪽 제거 둘 중 하나만 가능
+                return isPalindromeRange(s, l + 1, r) || isPalindromeRange(s, l, r - 1);
+            }
         }
+        return true;
+    }
 
+    private static boolean isPalindromeRange(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
         return true;
     }
 }
