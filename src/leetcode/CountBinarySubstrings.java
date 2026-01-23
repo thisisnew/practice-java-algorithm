63a69f1321eeaa456e1369da3f743e21673cf727
thisisnew <thisisnew@naver.com>
2026-01-23 19:58:09 +0900

    leetcode Count Binary Substrings

--- src/leetcode/CountBinarySubstrings.java
+++ src/leetcode/CountBinarySubstrings.java
@@ -11,26 +11,22 @@ public class CountBinarySubstrings {
     }
 
     private static int countBinarySubstrings(String s) {
+        var prevRun = 0;
+        var curRun = 1;
+        var ans = 0;
 
-        var zeroCnt = 0;
-        var oneCnt = 0;
-
-        for (var c : s.toCharArray()) {
-            if (c == '0') {
-                zeroCnt++;
+        for (var i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                curRun++;
             } else {
-                oneCnt++;
+                ans += Math.min(prevRun, curRun);
+                prevRun = curRun;
+                curRun = 1;
             }
         }
 
-        var min = Math.min(zeroCnt, oneCnt);
+        ans += Math.min(prevRun, curRun);
 
-        for (var c : s.toCharArray()) {
-
-        }
-
-
-
-        return 0;
+        return ans;
     }
 }
