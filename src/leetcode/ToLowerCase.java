48141375b9e65dafb6749c9fff789035749ac5dd
thisisnew <thisisnew@naver.com>
2026-01-23 19:59:23 +0900

    leetcode To Lower Case

---
+++ src/leetcode/ToLowerCase.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ToLowerCase {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(toLowerCase(br.readLine()));
+    }
+
+    private static String toLowerCase(String s) {
+        return "";
+    }
+}
