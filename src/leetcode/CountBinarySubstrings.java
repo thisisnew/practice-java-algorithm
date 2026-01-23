4cbff9c53a69d36e8da578cc28d194ca7a79e2fc
thisisnew <thisisnew@naver.com>
2026-01-23 19:54:16 +0900

    leetcode Count Binary Substrings

---
+++ src/leetcode/CountBinarySubstrings.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CountBinarySubstrings {
+    public static void main(String[] args) throws IOException {
+        var br =  new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(countBinarySubstrings(br.readLine()));
+    }
+
+    private static int countBinarySubstrings(String s) {
+
+        var zeroCnt = 0;
+        var oneCnt = 0;
+
+        for (var c : s.toCharArray()) {
+            if (c == '0') {
+                zeroCnt++;
+            } else {
+                oneCnt++;
+            }
+        }
+
+        var min = Math.min(zeroCnt, oneCnt);
+
+
+        return 0;
+    }
+}
