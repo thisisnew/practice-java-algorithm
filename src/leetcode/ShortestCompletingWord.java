3875e9e9705f12177282a01169b6315e539436c6
thisisnew <thisisnew@naver.com>
2026-01-25 20:41:55 +0900

    leetcode Shortest Completing Word

--- src/leetcode/ShortestCompletingWord.java
+++ src/leetcode/ShortestCompletingWord.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class ShortestCompletingWord {
     public static void main(String[] args) throws IOException {
@@ -8,6 +9,19 @@ public class ShortestCompletingWord {
     }
 
     private static String shortestCompletingWord(String licensePlate, String[] words) {
+
+        var map = new HashMap<Character, Integer>();
+
+        for (var c : licensePlate.toLowerCase().toCharArray()) {
+            if (Character.isLetter(c)) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        for (var word : words) {
+
+        }
+
         return "";
     }
 }
