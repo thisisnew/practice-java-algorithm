e795fcab9851b058f590edc3b89467864e8664fc
thisisnew <thisisnew@naver.com>
2026-01-25 20:35:46 +0900

    leetcode Shortest Completing Word

---
+++ src/leetcode/ShortestCompletingWord.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.io.IOException;
+
+public class ShortestCompletingWord {
+    public static void main(String[] args) throws IOException {
+        System.out.println(shortestCompletingWord("", new String[]{}));
+    }
+
+    private static String shortestCompletingWord(String licensePlate, String[] words) {
+        return "";
+    }
+}
