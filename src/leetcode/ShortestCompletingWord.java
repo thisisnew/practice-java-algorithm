c0613a08f5dacdcffad089ac9d4681cc43e856df
thisisnew <thisisnew@naver.com>
2026-01-28 19:46:07 +0900

    leetcode Shortest Completing Word

--- src/leetcode/ShortestCompletingWord.java
+++ src/leetcode/ShortestCompletingWord.java
@@ -27,7 +27,7 @@ public class ShortestCompletingWord {
             var len = list.size();
 
             for (var c : word.toCharArray()) {
-                if (Character.isLetter(c) && !list.contains(c)) {
+                if (!Character.isLetter(c) || !list.contains(c)) {
                     continue;
                 }
 
