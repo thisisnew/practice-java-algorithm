36d98b1671bd3baa22b31f81cf37d937d8aa20f8
thisisnew <thisisnew@naver.com>
2026-01-31 20:47:40 +0900

    leetcode Jewels and Stones

--- src/leetcode/JewelsAndStones.java
+++ src/leetcode/JewelsAndStones.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class JewelsAndStones {
     public static void main(String[] args) throws IOException {
@@ -8,6 +10,25 @@ public class JewelsAndStones {
     }
 
     private static int numJewelsInStones(String jewels, String stones) {
-        return 0;
+
+        var queue = new LinkedList<Character>();
+
+        for(var s : stones.toCharArray()) {
+            queue.add(s);
+        }
+
+        var result = 0;
+
+        for(var j : jewels.toCharArray()) {
+            var pop = queue.pop();
+
+            if (Objects.equals(j, pop)) {
+                result++;
+            } else {
+                queue.add(pop);
+            }
+        }
+
+        return result;
     }
 }
