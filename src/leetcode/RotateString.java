31bd77407b011f8e9399a43f820c80f41a1007f5
thisisnew <thisisnew@naver.com>
2026-02-01 15:45:38 +0900

    leetcode Rotate String

---
+++ src/leetcode/RotateString.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.io.IOException;
+
+public class RotateString {
+    public static void main(String[] args) throws IOException {
+        System.out.println(rotateString("",""));
+    }
+
+    private static boolean rotateString(String s, String goal) {
+        return true;
+    }
+}
