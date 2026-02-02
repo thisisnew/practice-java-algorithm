5ff124e9b400e595888f9d569195e26f3673b974
thisisnew <thisisnew@naver.com>
2026-02-02 18:59:59 +0900

    leetcode Rotate String

--- src/leetcode/RotateString.java
+++ src/leetcode/RotateString.java
@@ -1,7 +1,6 @@
 package leetcode;
 
 import java.io.IOException;
-import java.util.LinkedList;
 
 public class RotateString {
     public static void main(String[] args) throws IOException {
@@ -10,18 +9,15 @@ public class RotateString {
 
     private static boolean rotateString(String s, String goal) {
 
-        if (s.length() != goal.length()) return false;
-        if (s.equals(goal)) return true;
-
-        var queue = new LinkedList<>();
-
-        for (var c : s.toCharArray()) {
-            queue.add(c);
+        if (s.length() != goal.length()) {
+            return false;
         }
 
-        for (var i = 0; i < queue.size(); i++) {
+        if (s.equals(goal)) {
+            return true;
+        }
 
-
+        for (var i = 0; i < s.length(); i++) {
 
         }
 
