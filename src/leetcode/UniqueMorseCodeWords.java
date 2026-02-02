5a302ae6ba61163b829503489352afed5571c4d8
thisisnew <thisisnew@naver.com>
2026-02-02 19:12:35 +0900

    leetcode Unique Morse Code Words

---
+++ src/leetcode/UniqueMorseCodeWords.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.io.IOException;
+
+public class UniqueMorseCodeWords {
+    public static void main(String[] args) throws IOException {
+        System.out.println(uniqueMorseRepresentations(new String[]{}));
+    }
+
+    private static int uniqueMorseRepresentations(String[] words) {
+        return 0;
+    }
+}
