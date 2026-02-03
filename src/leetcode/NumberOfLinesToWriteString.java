5c4b790022c4be7a7011ff5c11f87075c186f2c1
thisisnew <thisisnew@naver.com>
2026-02-03 18:59:32 +0900

    leetcode Number of Lines To Write String

---
+++ src/leetcode/NumberOfLinesToWriteString.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class NumberOfLinesToWriteString {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.toString(numberOfLines(new int[]{}, "")));
+    }
+
+    private static int[] numberOfLines(int[] widths, String s) {
+        return new int[]{};
+    }
+}
