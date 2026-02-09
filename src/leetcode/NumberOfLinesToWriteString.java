ad32c492c745f7174b94a2ab44ca480bf29c01ca
thisisnew <thisisnew@naver.com>
2026-02-09 21:44:35 +0900

    leetcode Number of Lines To Write String

--- src/leetcode/NumberOfLinesToWriteString.java
+++ src/leetcode/NumberOfLinesToWriteString.java
@@ -10,7 +10,7 @@ public class NumberOfLinesToWriteString {
     private static final int MAX_PIXEL = 100;
 
     public static void main(String[] args) throws IOException {
-        System.out.println(Arrays.toString(numberOfLines(new int[]{4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}, "bbbcccdddaaa")));
+        System.out.println(Arrays.toString(numberOfLines(new int[]{10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10}, "abcdefghijklmnopqrstuvwxyz")));
     }
 
     private static int[] numberOfLines(int[] widths, String s) {
@@ -20,7 +20,7 @@ public class NumberOfLinesToWriteString {
         for (int i = 0; i < s.length(); i++) {
             var pixel = widths[s.charAt(i) - 'a'];
 
-            if (characters.sumPixels() >= MAX_PIXEL - pixel) {
+            if (characters.sumPixels() > MAX_PIXEL - pixel) {
                 characters.refresh();
             }
 
