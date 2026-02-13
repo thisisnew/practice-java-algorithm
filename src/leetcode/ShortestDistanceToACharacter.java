e4fb93feed8e9ed337c5920bb2a257e511d41a64
thisisnew <thisisnew@naver.com>
2026-02-14 08:44:27 +0900

    leetcode Shortest Distance to a Character

--- src/leetcode/ShortestDistanceToACharacter.java
+++ src/leetcode/ShortestDistanceToACharacter.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class ShortestDistanceToACharacter {
     public static void main(String[] args) throws IOException {
@@ -9,6 +10,15 @@ public class ShortestDistanceToACharacter {
     }
 
     private static int[] shortestToChar(String s, char c) {
+
+        var indexes = new HashSet<Integer>();
+
+        for (var i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'e') {
+                indexes.add(i);
+            }
+        }
+
         return new int[]{};
     }
 }
