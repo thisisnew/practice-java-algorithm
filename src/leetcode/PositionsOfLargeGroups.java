26f5090550a4d0e5cd997dce987b372368b87568
thisisnew <thisisnew@naver.com>
2026-02-17 07:51:11 +0900

    leetcode Positions of Large Groups

---
+++ src/leetcode/PositionsOfLargeGroups.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.io.IOException;
+import java.util.List;
+
+public class PositionsOfLargeGroups {
+    public static void main(String[] args) throws IOException {
+        System.out.println(largeGroupPositions(""));
+    }
+
+    private static List<List<Integer>> largeGroupPositions(String s) {
+        return List.of();
+    }
+}
