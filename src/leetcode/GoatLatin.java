a14ae65441387fd6a31e563464c76c17d89ed02f
thisisnew <thisisnew@naver.com>
2026-02-17 07:29:58 +0900

    leetcode Goat Latin

--- src/leetcode/GoatLatin.java
+++ src/leetcode/GoatLatin.java
@@ -8,6 +8,22 @@ public class GoatLatin {
     }
 
     private static String toGoatLatin(String sentence) {
+
+        var split = sentence.split(" ");
+        var aCnt = 1;
+        var sb = new StringBuilder();
+
+        for (var word : split) {
+            var ma = "m" + "a".repeat(aCnt++);
+
+            if (word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u")) {
+                sb.append(word).append(ma).append(" ");
+                continue;
+            }
+
+
+        }
+
         return "";
     }
 }
