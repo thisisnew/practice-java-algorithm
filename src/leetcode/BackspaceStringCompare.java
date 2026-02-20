45869edaa51bdc1810df334438d62f602e783055
thisisnew <thisisnew@naver.com>
2026-02-20 09:32:31 +0900

    leetcode Backspace String Compare

---
+++ src/leetcode/BackspaceStringCompare.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.io.IOException;
+
+public class BackspaceStringCompare {
+    public static void main(String[] args) throws IOException {
+        System.out.println(backspaceCompare("", ""));
+    }
+
+    private static boolean backspaceCompare(String s, String t) {
+        return false;
+    }
+}
