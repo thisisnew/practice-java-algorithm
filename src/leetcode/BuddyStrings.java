390b08fa093af95e706fc8e1a2c26b0ae410c662
thisisnew <thisisnew@naver.com>
2026-02-20 09:46:02 +0900

    leetcode Buddy Strings

---
+++ src/leetcode/BuddyStrings.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import java.io.IOException;
+
+public class BuddyStrings {
+    public static void main(String[] args) throws IOException {
+        System.out.println(buddyStrings("", ""));
+    }
+
+    private static boolean buddyStrings(String s, String goal) {
+        return false;
+    }
+}
