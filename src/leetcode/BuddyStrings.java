23ad9d24a3d199cdce4fe1864d4db7561b125665
thisisnew <thisisnew@naver.com>
2026-02-21 14:23:01 +0900

    leetcode Buddy Strings

--- src/leetcode/BuddyStrings.java
+++ src/leetcode/BuddyStrings.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class BuddyStrings {
     public static void main(String[] args) throws IOException {
@@ -8,6 +9,26 @@ public class BuddyStrings {
     }
 
     private static boolean buddyStrings(String s, String goal) {
-        return false;
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        var sArr = s.toCharArray();
+        var goalArr = goal.toCharArray();
+
+        for (var i = 0; i < sArr.length; i++) {
+            if (sArr[i] != goalArr[i]) {
+                for (var j = i; j < s.length(); j++) {
+                    if (sArr[j] == goalArr[j]) {
+                        var temp = sArr[i];
+                        sArr[i] = sArr[j];
+                        sArr[j] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return Arrays.toString(sArr).equals(goal);
     }
 }
