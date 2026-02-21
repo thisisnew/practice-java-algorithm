758c9b4c08c3e0649f0da308f73f2a336e49f8e2
thisisnew <thisisnew@naver.com>
2026-02-21 14:24:13 +0900

    leetcode Buddy Strings

--- src/leetcode/BuddyStrings.java
+++ src/leetcode/BuddyStrings.java
@@ -20,9 +20,7 @@ public class BuddyStrings {
             if (sArr[i] != goalArr[i]) {
                 for (var j = i; j < s.length(); j++) {
                     if (sArr[j] == goalArr[j]) {
-                        var temp = sArr[i];
-                        sArr[i] = sArr[j];
-                        sArr[j] = temp;
+                        switchCh(sArr, i, j);
                         break;
                     }
                 }
@@ -30,5 +28,11 @@ public class BuddyStrings {
         }
 
         return Arrays.toString(sArr).equals(goal);
+    }
+
+    private static void switchCh(char[] arr, int i, int j) {
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
