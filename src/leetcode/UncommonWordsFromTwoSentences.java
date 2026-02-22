74f914ed7f8fc953f2b95af99d032f89d9151fb4
thisisnew <thisisnew@naver.com>
2026-02-22 14:44:53 +0900

    leetcode Uncommon Words from Two Sentences

---
+++ src/leetcode/UncommonWordsFromTwoSentences.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class UncommonWordsFromTwoSentences {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Arrays.toString(uncommonFromSentences("", "")));
+    }
+
+    private static String[] uncommonFromSentences(String s1, String s2) {
+        return new String[]{};
+    }
+}
