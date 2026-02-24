a933fb04c4afcfa0b1490fcb6042a13b9bb3a580
thisisnew <thisisnew@naver.com>
2026-02-24 18:32:23 +0900

    leetcode Uncommon Words from Two Sentences

--- src/leetcode/UncommonWordsFromTwoSentences.java
+++ src/leetcode/UncommonWordsFromTwoSentences.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class UncommonWordsFromTwoSentences {
     public static void main(String[] args) throws IOException {
@@ -15,32 +16,25 @@ public class UncommonWordsFromTwoSentences {
         var s1Arr = s1.split(" ");
         var s2Arr = s2.split(" ");
 
-        var result = new ArrayList<String>();
-
         if (s1Arr.length > s2Arr.length) {
-            var words = new HashSet<>(Arrays.asList(s2Arr));
-
-            for (var word: s1Arr) {
-                if (words.contains(word)) {
-                    continue;
-                }
-
-                result.add(word);
-            }
-
-        } else {
-
-            var words = new HashSet<>(Arrays.asList(s1Arr));
-
-            for (var word: s2Arr) {
-                if (words.contains(word)) {
-                    continue;
-                }
-
-                result.add(word);
-            }
+            return uncommonWords(s1Arr, s2Arr).toArray(new String[0]);
         }
 
-        return result.toArray(new String[0]);
+        return uncommonWords(s2Arr, s1Arr).toArray(new String[0]);
+    }
+
+    private static List<String> uncommonWords(String[] s1Arr, String[] s2Arr) {
+        var result = new ArrayList<String>();
+        var words = new HashSet<>(Arrays.asList(s2Arr));
+
+        for (var word: s1Arr) {
+            if (words.contains(word)) {
+                continue;
+            }
+
+            result.add(word);
+        }
+
+        return result;
     }
 }
