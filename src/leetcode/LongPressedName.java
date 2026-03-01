17a0ed76edb31ae504590e04a00ee1053587fddb
thisisnew <thisisnew@naver.com>
2026-03-02 08:44:17 +0900

    leetcode Long Pressed Name

--- src/leetcode/LongPressedName.java
+++ src/leetcode/LongPressedName.java
@@ -8,6 +8,24 @@ public class LongPressedName {
     }
 
     private static boolean isLongPressedName(String name, String typed) {
+
+        var offset = 0;
+
+        while(offset < name.length()) {
+            var c = name.charAt(offset);
+
+            if (offset == name.length() - 1) {
+
+            }
+
+            if (c == name.charAt(offset +1)) {
+                offset++;
+            }
+
+
+        }
+
+
         return false;
     }
 }
