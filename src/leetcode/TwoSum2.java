9fe361a4a556ffa70b1aa8ffca2552a41cb085a6
thisisnew <thisisnew@naver.com>
2026-03-29 17:50:18 +0900

    leetcode two sum 2

--- src/leetcode/TwoSum2.java
+++ src/leetcode/TwoSum2.java
@@ -16,8 +16,8 @@ public class TwoSum2 {
         for (int i=0; i<numbers.length-1; i++) {
             for (int j=i+1; j<numbers.length; j++) {
                 if (numbers[i] + numbers[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
+                    result[0] = i+1;
+                    result[1] = j+1;
                     resolved = true;
                     break;
                 }
