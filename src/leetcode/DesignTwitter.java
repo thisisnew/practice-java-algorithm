d0ee427be05998871ef598954c3bb80b64e2d99c
thisisnew <thisisnew@naver.com>
2026-04-06 21:03:57 +0900

    leetcode Design Twitter

--- src/leetcode/DesignTwitter.java
+++ src/leetcode/DesignTwitter.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class DesignTwitter {
@@ -13,14 +14,18 @@ public class DesignTwitter {
 
 class Twitter {
 
+    private Map<Integer, Set<Feed>> feed;
     private Map<Integer, Set<Integer>> follower;
 
     public Twitter() {
+        this.feed = new HashMap<>();
         this.follower = new HashMap<>();
     }
 
     public void postTweet(int userId, int tweetId) {
-
+        var userFeeds = Objects.requireNonNullElse(this.feed.get(userId), new HashSet<Feed>());
+        userFeeds.add(new Feed(tweetId));
+        this.feed.put(userId, userFeeds);
     }
 
     public List<Integer> getNewsFeed(int userId) {
@@ -37,5 +42,33 @@ class Twitter {
         var followers = Objects.requireNonNullElse(follower.get(followerId), new HashSet<Integer>());
         followers.remove(followeeId);
         follower.put(followerId, followers);
+    }
+
+    private static class Feed {
+        private int tweetId;
+        private LocalDateTime timestamp;
+
+        public Feed(int tweetId) {
+            this.tweetId = tweetId;
+            this.timestamp = LocalDateTime.now();
+        }
+
+        public LocalDateTime timestamp() {
+            return timestamp;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+
+            if (obj == null || obj.getClass() != this.getClass()) {
+                return false;
+            }
+
+            Feed other = (Feed) obj;
+            return this.tweetId == other.tweetId;
+        }
     }
 }
