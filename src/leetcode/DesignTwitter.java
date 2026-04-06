706fa32a4bdfd5eb66e96a7a8d814ca7d2dfb635
thisisnew <thisisnew@naver.com>
2026-04-06 20:49:48 +0900

    leetcode Design Twitter

---
+++ src/leetcode/DesignTwitter.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DesignTwitter {
+    public static void main(String[] args) throws IOException {
+        var twitter = new Twitter();
+
+
+    }
+}
+
+class Twitter {
+
+    public Twitter() {
+
+    }
+
+    public void postTweet(int userId, int tweetId) {
+
+    }
+
+    public List<Integer> getNewsFeed(int userId) {
+        return List.of();
+    }
+
+    public void follow(int followerId, int followeeId) {
+
+    }
+
+    public void unfollow(int followerId, int followeeId) {
+
+    }
+}
