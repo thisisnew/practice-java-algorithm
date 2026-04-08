f72ff22c8e4e0edfc4c2274a98ae5b5aa3fb6cd4
thisisnew <thisisnew@naver.com>
2026-04-08 20:48:43 +0900

    leetcode Underground System

--- src/leetcode/UndergroundSystem.java
+++ src/leetcode/UndergroundSystem.java
@@ -1,26 +1,35 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class UndergroundSystem {
 
-    private final Map<String, Station> checkInStation;
-    private final Map<String, Station> checkOutStation;
+    private final Map<Integer, Station> checkInStation;
+    private final Map<String, TravelTime> travelTime;
 
     public UndergroundSystem() {
         this.checkInStation = new HashMap<>();
-        this.checkOutStation = new HashMap<>();
+        this.travelTime = new HashMap<>();
     }
 
     public void checkIn(int id, String stationName, int t) {
-        Station station = new Station(id, stationName, t);
-        checkInStation.put(stationName, station);
+        Station station = new Station(stationName, t);
+        checkInStation.put(id, station);
     }
 
     public void checkOut(int id, String stationName, int t) {
-        Station station = new Station(id, stationName, t);
-        checkOutStation.put(stationName, station);
+        var checkInStation = this.checkInStation.get(id);
+        if (checkInStation == null) {
+            return;
+        }
+
+        String startStation = checkInStation.stationName();
+        String travelStation = startStation + "-" + stationName;
+        TravelTime travelTime = this.travelTime.getOrDefault(travelStation, new TravelTime(new ArrayList<>()));
+        travelTime.addTime(t - checkInStation.time());
     }
 
     public double getAverageTime(String startStation, String endStation) {
@@ -31,13 +40,31 @@ public class UndergroundSystem {
 
 class Station {
 
-    private final int id;
     private final String stationName;
     private final int time;
 
-    Station(int id, String stationName, int time) {
-        this.id = id;
+    Station(String stationName, int time) {
         this.stationName = stationName;
         this.time = time;
+    }
+
+    public String stationName() {
+        return stationName;
+    }
+
+    public int time() {
+        return time;
+    }
+}
+
+class TravelTime {
+    private final List<Integer> times;
+
+    TravelTime(List<Integer> times) {
+        this.times = times;
+    }
+
+    public void addTime(int time) {
+        this.times.add(time);
     }
 }
