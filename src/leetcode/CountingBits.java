f732b101c7ef0d8e8aa885b28212dbee91ebb81d
thisisnew <thisisnew@naver.com>
2026-04-12 15:35:04 +0900

    leetcode Counting Bits

--- src/leetcode/CountingBits.java
+++ src/leetcode/CountingBits.java
@@ -2,15 +2,15 @@ package leetcode;
 
 public class CountingBits {
     public int[] countBits(int n) {
-        int[] result = new int[n];
+        int[] result = new int[n+1];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             int temp = i;
             int count = 0;
 
-            while(temp > 0) {
+            while (temp > 0) {
+                count += temp % 2;
                 temp = temp / 2;
-                count++;
             }
 
             result[i] = count;
