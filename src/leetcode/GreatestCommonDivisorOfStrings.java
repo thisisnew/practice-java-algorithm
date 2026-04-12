7062b80b9849176ba854fbdc80459fb2a08fdb32
thisisnew <thisisnew@naver.com>
2026-04-12 13:44:36 +0900

    leetcode Greatest Common Divisor of Strings

---
+++ src/leetcode/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class GreatestCommonDivisorOfStrings {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+
+    }
+}
