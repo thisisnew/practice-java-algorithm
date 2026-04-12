d9928811a9d8dc223b6a5dff6bf23b2d55387584
thisisnew <thisisnew@naver.com>
2026-04-12 14:25:35 +0900

    leetcode Find the Highest Altitude

--- src/leetcode/FindTheHighestAltitude.java
+++ src/leetcode/FindTheHighestAltitude.java
@@ -2,16 +2,16 @@ package leetcode;
 
 public class FindTheHighestAltitude {
     public int largestAltitude(int[] gain) {
-        int max = Integer.MIN_VALUE;
+        int currentAltitude = 0;
+        int maxAltitude = 0;
 
-        for (int i = 0; i<gain.length - 1; i++) {
-            int diff = gain[i] + gain[i + 1];
-
-            if (diff > max) {
-                max = diff;
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            if (currentAltitude > maxAltitude) {
+                maxAltitude = currentAltitude;
             }
         }
 
-        return max;
+        return maxAltitude;
     }
 }
