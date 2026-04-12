fbffe1b21cffeed7e9e55b0a5ce597b5d606af1e
thisisnew <thisisnew@naver.com>
2026-04-12 13:02:46 +0900

    leetcode Can Place Flowers

--- src/leetcode/CanPlaceFlowers.java
+++ src/leetcode/CanPlaceFlowers.java
@@ -10,27 +10,35 @@ public class CanPlaceFlowers {
 
         for (int i = 0; i < flowerbed.length; i++) {
 
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+
             if (n == 0) {
                 return true;
             }
 
             if (i == 0) {
                 if (flowerbed.length == 1) {
+                    flowerbed[i] = 1;
                     n--;
                     return n == 0;
                 }
 
+                flowerbed[i] = 1;
                 n--;
                 continue;
             }
 
             if (i == flowerbed.length - 1) {
                 if (flowerbed[i - 1] == 0) {
+                    flowerbed[i] = 1;
                     n--;
                 }
             }
 
             if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
                 n--;
             }
 
