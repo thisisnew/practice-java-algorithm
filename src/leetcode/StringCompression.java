d1be51b78e6a148101bac7ad945538d0279d19ed
thisisnew <thisisnew@naver.com>
2026-04-13 21:36:32 +0900

    leetcode String Compression

--- src/leetcode/StringCompression.java
+++ src/leetcode/StringCompression.java
@@ -1,36 +1,50 @@
 package leetcode;
 
 public class StringCompression {
+
+    public static void main(String[] args) {
+        System.out.println(new StringCompression().compress(new char[] {'a', 'a', 'b', 'b', 'c', 'c', 'c'}));
+    }
+
     public int compress(char[] chars) {
 
-        StringBuilder sb = new StringBuilder();
         char prev = chars[0];
         int count = 1;
+        int readIdx = 1;
+        int writeIdx = 0;
 
-        for (int i = 1; i < chars.length; i++) {
-            char c = chars[i];
+        while (readIdx < chars.length) {
+            char c = chars[readIdx];
 
             if (c == prev) {
+                readIdx++;
                 count++;
                 continue;
             }
 
-            sb.append(prev);
+            chars[writeIdx++] = prev;
 
             if (count > 1) {
-                sb.append(count);
+                String countString = String.valueOf(count);
+                for (int i = 0; i < countString.length(); i++) {
+                    chars[writeIdx++] = countString.charAt(i);
+                }
             }
 
             prev = c;
             count = 1;
+            readIdx++;
         }
 
-        sb.append(prev);
+        chars[writeIdx++] = prev;
 
         if (count > 1) {
-            sb.append(count);
+            String countString = String.valueOf(count);
+            for (int i = 0; i < countString.length(); i++) {
+                chars[writeIdx++] = countString.charAt(i);
+            }
         }
 
-        return sb.length();
+        return writeIdx;
     }
 }
