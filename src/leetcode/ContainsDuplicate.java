2be888fa1dd00e7aaddaf2855d5a23a0ff06e2b9
thisisnew <thisisnew@naver.com>
2026-08-25 21:43:14 +0900

    leet code : Contains Duplicate

--- src/leetcode/ContainsDuplicate.java
+++ src/leetcode/ContainsDuplicate.java
@@ -1,23 +1,22 @@
 package leetcode;
 
 import java.util.HashSet;
-import java.util.Set;
 
 public class ContainsDuplicate {
-
-    private static final Set<Integer> SET = new HashSet<>();
 
     public static void main(String[] args) {
         System.out.println(containsDuplicate(new int[]{1,2,3,4}));
     }
 
     private static boolean containsDuplicate(int[] nums) {
+        var set = new HashSet<>();
+
         for (var num: nums) {
-            if (SET.contains(num)) {
+            if (set.contains(num)) {
                 return true;
             }
 
-            SET.add(num);
+            set.add(num);
         }
 
         return false;
