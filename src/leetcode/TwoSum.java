5f3a81bd66d04fc3ae2376f6fa293e1a29167f0a
thisisnew <thisisnew@naver.com>
2026-08-26 20:57:45 +0900

    leet code : Twon Sum

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -6,22 +6,21 @@ import java.util.Map;
 
 public class TwoSum {
 
-    private static final Map<Integer, Integer> MAP = new HashMap<>();
-
     public static void main(String[] args) {
         System.out.println(Arrays.toString(twoSum(new int[]{2, 7, 11, 15}, 9)));
     }
 
     private static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            var v = target - nums[i];
+            var value = target - nums[i];
 
-            if (MAP.containsKey(v)) {
-                return new int[]{MAP.get(v), i};
+            if (map.containsKey(value)) {
+                return new int[]{i , map.get(value)};
             }
 
-            MAP.put(nums[i], i);
+            map.put(nums[i], i);
         }
 
         return new int[]{};
